0c9d3dd86ab6f7fd077edf21cac4b6fe261e5a7e
Renu Parashar <renu@vedang.net>
2020-08-29 19:14:43 -0700

    New Encroachment module added

---
+++ Encroachment/Controllers/WatchWardController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_Encrochment_Management.Controllers
+{
+    public class WatchWardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult WatchWardApproval()
+        {
+            return View();
+        }
+
+        public IActionResult WatchWardApprovalCreate()
+        {
+            return View();
+        }
+    }
+}
