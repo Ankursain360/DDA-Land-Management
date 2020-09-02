b3e786f2cd8f55c3e26dcce9d9292068f9aad2ce
Renu Parashar <renu@vedang.net>
2020-09-02 13:19:19 -0700

    Demolition Layout Added And page

--- Demolition/Startup.cs
+++ Demolition/Startup.cs
@@ -1,12 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+//using Demolition.Common;
+using Demolition.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using BotDetect.Web;
+using Newtonsoft.Json.Serialization;
+using DDAPropertyREG.Models;
 using Microsoft.Extensions.Hosting;
 
 namespace Demolition
@@ -23,7 +34,40 @@ namespace Demolition
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<IFileProvider>(
+            new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
+            services.AddDbContext<lmsContext>(a => a.UseMySQL(Configuration.GetSection("ConnectionString:Con").Value));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+          //  services.AddMvc()
+         //  .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+         //   services.AddSession();
+         //   services.AddMvc();
+         //   services.AddMvc().AddSessionStateTempDataProvider();
+         //   services.AddSingleton<ITempDataProvider, CookieTempDataProvider>();
+         //   services.AddMvc().AddViewOptions(options =>
+         //   {
+         //    //   options.SuppressTempDataAttributePrefix = true;
+         //   });
+            services.Configure<CookieTempDataProviderOptions>(options =>
+            {
+                options.Cookie.Name = "MyTempDataCookie";
+            });
+           // services.AddScoped<ILogger, Logger>();
+            // Add Session services.
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(20);
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,7 +94,7 @@ namespace Demolition
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Create}/{id?}");
             });
         }
     }
