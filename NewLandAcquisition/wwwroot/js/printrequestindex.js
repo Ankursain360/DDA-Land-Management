b42867c1d4aeea3b5a4dc5869ff6a6a440552e5e
ishu rana <ishu.rana@vedang.net>
2020-09-03 19:15:11 +0530

    print request js file added

---
+++ NewLandAcquisition/wwwroot/js/printrequestindex.js
@@ -0,0 +1,18 @@
+﻿
+
+
+    $(function () {
+        $("input[name='search']").click(function () {
+            if ($("#searchname").is(":checked")) {
+                $("#divname").show();
+                $("#divfile").hide();
+                $("#DivForGenerate").show();
+            }
+            if ($("#searchno").is(":checked")) {
+                $("#divfile").show();
+                $("#divname").hide();
+                $("#DivForGenerate").show();
+
+            }
+        });
+    });
