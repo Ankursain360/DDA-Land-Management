258ede4cd7d89498a90b3b12b03b59fccd50b1a7
Renu Parashar <renu@vedang.net>
2020-09-03 13:32:02 -0700

    Damge Payee Name related error resolved

---
+++ DamagePayee/Controllers/NoticeGenerationReportController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DamagePayee.Controllers
+{
+    public class NoticeGenerationReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int id)
+        {
+            ViewBag.IsShowData = "Yes";
+            return View();
+        }
+
+    }
+}
---
+++ DamagePayee/Controllers/PaymentTransactionReportController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DamagePayee.Controllers
+{
+    public class PaymentTransactionReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int id)
+        {
+            ViewBag.IsShowData = "Yes";
+            return View();
+        }
+    }
+}
