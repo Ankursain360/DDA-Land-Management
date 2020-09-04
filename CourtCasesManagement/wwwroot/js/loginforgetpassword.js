c040d4c2c7652d2fd42adbed83582b53e0f7eeb2
ishu rana <ishu.rana@vedang.net>
2020-09-04 11:54:39 +0530

    installed packages and added wwwroot folder ,login dashboard and layout pages in cour case management module done

---
+++ CourtCasesManagement/wwwroot/js/loginforgetpassword.js
@@ -0,0 +1,20 @@
+﻿
+    $(document).ready(function () {
+        var value = document.getElementById('loginname').value;
+        if (value === '') {
+        $("#loginname").focus();
+        }
+
+    });
+
+    $('#submit').click(function () {
+        var value = document.getElementById('loginname').value;
+        if (value === '') {
+        //alert('Enter your name');
+        $("#loginname").focus();
+            //$("#error").show('slow');
+            //$("p").html("focus event triggered");
+        }
+    })
+
+
