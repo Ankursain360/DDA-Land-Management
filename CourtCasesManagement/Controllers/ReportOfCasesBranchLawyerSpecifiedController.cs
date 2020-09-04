eb17ee7204bb2f89b9ab595ccc4bf1718ca8978c
ishu rana <ishu.rana@vedang.net>
2020-09-04 20:06:28 +0530

    changes done in Layout of new land acquisition module and 6 pages added in court case mnagement module

---
+++ CourtCasesManagement/Controllers/ReportOfCasesBranchLawyerSpecifiedController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourtCasesManagement.Controllers
+{
+    public class ReportOfCasesBranchLawyerSpecifiedController : Controller
+    {
+        public IActionResult RptCasesBranchLawyerSpecified()
+        {
+            return View();
+        }
+    }
+}
---
+++ CourtCasesManagement/Controllers/ReportOfCasesBranchWiseController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourtCasesManagement.Controllers
+{
+    public class ReportOfCasesBranchWiseController : Controller
+    {
+        public IActionResult RptCasesBranchWise()
+        {
+            return View();
+        }
+    }
+}
