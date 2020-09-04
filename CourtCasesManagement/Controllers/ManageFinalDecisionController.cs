a88714c9d1f4c50e0cd445a855886841b7f4041e
ishu rana <ishu.rana@vedang.net>
2020-09-04 13:40:37 +0530

    another 5 pages added

---
+++ CourtCasesManagement/Controllers/ManageFinalDecisionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using DDAPropertyREG.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourtCasesManagement.Controllers
+{
+    public class ManageFinalDecisionController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
