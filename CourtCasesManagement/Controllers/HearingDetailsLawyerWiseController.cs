3e22ef58398c3ff7a80a0d9b7ffa3a2a7f5aca31
ishu rana <ishu.rana@vedang.net>
2020-09-04 17:06:45 +0530

    6 more pages added

---
+++ CourtCasesManagement/Controllers/HearingDetailsLawyerWiseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourtCasesManagement.Controllers
+{
+    public class HearingDetailsLawyerWiseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult HearingDetailsLawyerWise()
+        {
+            return View();
+        }
+    }
+}
