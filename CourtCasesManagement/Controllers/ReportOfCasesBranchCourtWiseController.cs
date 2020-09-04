720af93f1420b31c6bffd27661938d780e9a63ca
shalinirair <shalini.rai@vedang.net>
2020-09-04 18:09:57 +0530

    LegalDettails,Listofpendingcases,yearwisereportcourtwise,reportyearwise added

---
+++ CourtCasesManagement/Controllers/ReportOfCasesBranchCourtWiseController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourtCasesManagement.Controllers
+{
+    public class ReportOfCasesBranchCourtWiseController : Controller
+    {
+        public IActionResult RptCasesBranchCourtWise()
+        {
+            return View();
+        }
+    }
+}
