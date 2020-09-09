7665e40bbb8ca28b1c1aa1f8a0b426ba4f0ce5f2
Ashutosh <Ashutosh-Kumar.SHARMA@idemia.com>
2020-09-09 18:24:39 +0530

    Added Base controller and paging config dto

---
+++ Libraries/Dto/Common/PagingConfigDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Dto.Common
+{
+    public class PagingConfigDto
+    {
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+    }
+}
---
+++ SiteMaster/Controllers/CountryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SiteMaster.Controllers
+{
+    public class CountryController : BaseController
+    {
+        public CountryController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
