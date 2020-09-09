62d5b8077a0125c798b0e6b1f38b725e545e6e82
angelnikitasingh2021@gmail.com <angelnikitasingh2021@gmail.com>
2020-09-09 14:10:26 +0530

    add role

--- Libraries/Model/DataContext.cs
+++ Libraries/Model/DataContext.cs
@@ -22,6 +22,7 @@ namespace Libraries.Model
         public virtual DbSet<Zone> Zone { get; set; }
         public virtual DbSet<Village> Village { get; set; }
         public virtual DbSet<Division> Division { get; set; }
+        public virtual DbSet<Role> Role { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new SystemUserConfiguration());
@@ -31,6 +32,7 @@ namespace Libraries.Model
             modelBuilder.ApplyConfiguration(new DistrictConfiguration());
             modelBuilder.ApplyConfiguration(new VillageConfiguration());
             modelBuilder.ApplyConfiguration(new DivisionConfiguration());
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
 
             modelBuilder.ApplyConfiguration(new ModuleConfiguration());
 
