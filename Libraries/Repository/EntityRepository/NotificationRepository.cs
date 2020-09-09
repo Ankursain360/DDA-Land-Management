dfeb756fda9d9037f54722409ea23d5982737a9c
Renu Parashar <renu@vedang.net>
2020-09-09 14:02:04 -0700

    Notification class added in library

---
+++ Libraries/Repository/EntityRepository/NotificationRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Libraries.Model;
+using Libraries.Model.Entity;
+using Libraries.Repository.Common;
+using Libraries.Repository.IEntityRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Libraries.Repository.EntityRepository
+{
+    public class NotificationRepository : GenericRepository<Notification>, INotificationRepository
+    {
+
+        public NotificationRepository(DataContext dbContext) : base(dbContext)
+        {
+
+        }
+
+
+        public async Task<bool> Any(int id, string name)
+        {
+            return await _dbContext.Designation.AnyAsync(t => t.Id != id && t.Name.ToLower() == name.ToLower());
+        }
+    }
+
+
+}
---
+++ Libraries/Service/IApplicationService/INotificationService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Libraries.Model.Entity;
+using Libraries.Service.Common;
+
+
+namespace Libraries.Service.IApplicationService
+{
+    public interface INotificationService : IEntityService<Notification>
+    {
+        Task<List<Notification>> GetAllNotification(); // To Get all data added by renu
+
+        Task<bool> Update(int id, Notification notification); // To Upadte Particular data added by renu
+
+        Task<bool> Create(Notification notification);
+
+        Task<Notification> FetchSingleResult(int id);  // To fetch Particular data added by renu
+
+        Task<bool> Delete(int id);    // To Delete Data  added by renu
+
+        Task<bool> CheckUniqueName(int id, string notification);   // To check Unique Value  for notification
+    }
+}
