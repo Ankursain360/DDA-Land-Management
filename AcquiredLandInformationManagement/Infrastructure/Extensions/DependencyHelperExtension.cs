9a58bfdcd12141bf54a6de8334a73efdd40ff1ee
angelnikitasingh2021@gmail.com <angelnikitasingh2021@gmail.com>
2020-09-17 11:00:25 +0530

    Add data context

--- AcquiredLandInformationManagement/Infrastructure/Extensions/DependencyHelperExtension.cs
+++ AcquiredLandInformationManagement/Infrastructure/Extensions/DependencyHelperExtension.cs
@@ -30,6 +30,8 @@ namespace AcquiredLandInformationManagement.Infrastructure.Extensions
             services.AddScoped<IUnderSection4PlotRepository, Undersection4plotRepository>();
 
             services.AddScoped<IUndersection22Repository, Undersection22Repository>();
+            services.AddScoped<IAwardplotDetailsRepository, AwardplotDetailsRepository>();
+
 
 
             /* Application Services */
@@ -44,6 +46,8 @@ namespace AcquiredLandInformationManagement.Infrastructure.Extensions
             services.AddScoped<IKhasraService, KhasraService>();
             services.AddScoped<IUndersection4PlotService, Undersection4PlotService>();
             services.AddScoped<IUndersection22Service, Undersection22Service>();
+            services.AddScoped<IAwardplotDetailService, AwardplotDetailsService>();
+
         }
     }
 }
--- Libraries/Model/DataContext.cs
+++ Libraries/Model/DataContext.cs
@@ -53,6 +53,9 @@ namespace Libraries.Model
         public virtual DbSet<Proposalplotdetails> Proposalplotdetails { get; set; }
         public virtual DbSet<Undersection17> Undersection17 { get; set; }
         public virtual DbSet<Undersection22> Undersection22 { get; set; }
+        public virtual DbSet<Awardmasterdetail> Awardmasterdetail { get; set; }
+        public virtual DbSet<Awardplotdetails> Awardplotdetails { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new SystemUserConfiguration());
@@ -85,8 +88,10 @@ namespace Libraries.Model
             modelBuilder.ApplyConfiguration(new Undersection17Configuration());
             modelBuilder.ApplyConfiguration(new ProposalplotdetailsConfiguration());
             modelBuilder.ApplyConfiguration(new Undersection22Configuration());
-            
+            modelBuilder.ApplyConfiguration(new AwardplotDetailsConfiguration());
+
+
         }
-        
+
     }
 }
