1ffbb90bda444493b7e41601907ec9cf1c464b8d
Renu Parashar <renu@vedang.net>
2020-09-18 13:10:32 -0700

    packages and common functionlity added into LandTransfer module

---
+++ LandTransfer/Infrastructure/Extensions/DependencyHelperExtension.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.DependencyInjection;
+using Libraries.Repository.IEntityRepository;
+using Libraries.Repository.EntityRepository;
+using Libraries.Service.IApplicationService;
+using Libraries.Service.ApplicationService;
+using Libraries.Repository.Common;
+using Service.ApplicationService;
+
+namespace LandTransfer.Infrastructure.Extensions
+{
+    public static class DependencyHelperExtension
+    {
+        public static void RegisterDependency(this IServiceCollection services)
+		{
+            /* Common Dependencies */
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            /* Respository */
+            services.AddScoped<INazullandRepository, NazullandRepository>();
+
+            /* Application Services */
+            services.AddScoped<INazullandService, NazullandService>();
+        }
+    }
+}
