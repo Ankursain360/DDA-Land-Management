3564d78d07bdb4702bf54b045419c5a371057138
Renu Parashar <renu@vedang.net>
2020-09-18 11:44:20 -0700

    Land Use Master page added repository,services

---
+++ SiteMaster/Controllers/LandUseController.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Libraries.Model.Entity;
+using Libraries.Service.IApplicationService;
+using SiteMaster.Models;
+using Notification;
+using Notification.Constants;
+using Notification.OptionEnums;
+using Microsoft.AspNetCore.Authorization;
+using Dto.Search;
+
+namespace SiteMaster.Controllers
+{
+    public class LandUseController : Controller
+    {
+
+        private readonly ILandUseService _landuseService;
+
+        public LandUseController(ILandUseService landuseService)
+        {
+            _landuseService = landuseService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await _landuseService.GetAllLandUse();
+            return View(result);
+        }
+
+        //[HttpPost]
+        //public async Task<PartialViewResult> List(LandUseSearchDto model)
+        //{
+        //    var x = model.Name;
+        //    var result = await _landuseService.GetAllLandUse();
+        //    return PartialView("_List", result);
+        //}
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Landuse landuse)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var result = await _landuseService.Create(landuse);
+
+                    if (result == true)
+                    {
+                        ViewBag.Message = Alert.Show(Messages.AddRecordSuccess, "", AlertType.Success);
+                        return View();
+                    }
+                    else
+                    {
+                        ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                        return View(landuse);
+
+                    }
+                }
+                else
+                {
+                    return View(landuse);
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                return View(landuse);
+            }
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var Data = await _landuseService.FetchSingleResult(id);
+            if (Data == null)
+            {
+                return NotFound();
+            }
+            return View(Data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Landuse landuse)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var result = await _landuseService.Update(id, landuse);
+                    if (result == true)
+                    {
+                        ViewBag.Message = Alert.Show(Messages.UpdateRecordSuccess, "", AlertType.Success);
+                        var result1 = await _landuseService.GetAllLandUse();
+                        return View("Index", result1);
+                    }
+                    else
+                    {
+                        ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                        return View(landuse);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            return View(landuse);
+        }
+
+        [AcceptVerbs("Get", "Post")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Exist(int Id, string Name)
+        {
+            var result = await _landuseService.CheckUniqueName(Id, Name);
+            if (result == false)
+            {
+                return Json(true);
+            }
+            else
+            {
+                return Json($"LandUse: {Name} already exist");
+            }
+        }
+
+
+        public async Task<IActionResult> Delete(int id)  //Not in use
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            var form = await _landuseService.Delete(id);
+            if (form == false)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Message = Alert.Show(Messages.DeleteSuccess, "", AlertType.Success);
+            return View(form);
+        }
+
+        public async Task<IActionResult> DeleteConfirmed(int id)  // Used to Perform Delete Functionality added by Renu
+        {
+
+            var result = await _landuseService.Delete(id);
+            if (result == true)
+            {
+                ViewBag.Message = Alert.Show(Messages.DeleteSuccess, "", AlertType.Success);
+                var result1 = await _landuseService.GetAllLandUse();
+                return View("Index", result1);
+            }
+            else
+            {
+                ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                var result1 = await _landuseService.GetAllLandUse();
+                return View("Index", result1);
+            }
+
+        }
+
+        public async Task<IActionResult> View(int id)
+        {
+            var Data = await _landuseService.FetchSingleResult(id);
+            if (Data == null)
+            {
+                return NotFound();
+            }
+            return View(Data);
+        }
+    }
+}
