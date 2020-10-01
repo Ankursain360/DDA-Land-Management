ff018689467de0ce3e64b09113e175b18109abb8
shalini rai r <shalini.rai@vedang.net>
2020-10-01 07:56:08 -0700

    Us6 us17 us22 us4 added

---
+++ LIMSPublicInterface/Controllers/US17Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LIMSPublicInterface.Controllers
+{
+    public class US17Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(int id)
+        {
+            ViewBag.IsShowData = "Yes";
+            return View();
+        }
+    }
+}
