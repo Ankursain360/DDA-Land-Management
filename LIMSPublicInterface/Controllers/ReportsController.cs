3bffad914593319154c82a83383312905ff989cf
ishu rana <ishu.rana@vedang.net>
2020-10-03 11:28:58 +0530

    Award Possession page done

---
+++ LIMSPublicInterface/Controllers/ReportsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LIMSPublicInterface.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult VillageReport()
+        {
+            return View();
+        }
+        public IActionResult NazulVillageReport()
+        {
+            return View();
+        }
+        public IActionResult AcquiredVillageReport()
+        {
+            return View();
+        }
+        public IActionResult VillageDetailsKhasraWiseReport()
+        {
+            return View();
+        }
+       
+    }
+}
