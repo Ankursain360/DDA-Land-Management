6dfc46aff7613dc4a17f8b9a3cef42af002ac904
angelnikitasingh2021@gmail.com <angelnikitasingh2021@gmail.com>
2020-10-07 17:59:06 +0530

    commit for sync

--- LandTransfer/Controllers/ReportofLandTransferDepartmentWiseController.cs
+++ LandTransfer/Controllers/ReportofLandTransferDepartmentWiseController.cs
@@ -2,12 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Libraries.Model.Entity;
+using Libraries.Service.IApplicationService;
 using Microsoft.AspNetCore.Mvc;
+using Notification;
+using Notification.Constants;
+using Notification.OptionEnums;
 
 namespace LandTransfer.Controllers
 {
     public class ReportofLandTransferDepartmentWiseController : Controller
     {
+        private readonly ILandTransferService _landTransferService;
+
+        public ReportofLandTransferDepartmentWiseController(ILandTransferService landTransferService)
+        {
+            _landTransferService = landTransferService;
+        }
+
+        //async Task GetAllDepartment(Landtransfer landtransfer)
+        //{
+        //    //  propertyregistration.ZoneList = await _propertyregistrationService.GetZoneDropDownList();
+        //    landtransfer.departmentList = await _propertyregistrationService.GetDepartmentDropDownList();
+        //    //propertyregistration.DivisionList = await _propertyregistrationService.GetDivisionDropDownList();
+        //}
+
         public IActionResult Index()
         {
             return View();
