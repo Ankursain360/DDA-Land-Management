81624c5efd4fb8c454e861200203f32bf3c41fbd
shalini rai r <shalini.rai@vedang.net>
2020-10-06 23:23:23 -0700

    actions view added

---
+++ Libraries/Model/Entity/Actions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Libraries.Model.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libraries.Model.Entity
+{
+    public class Actions : AuditableEntity<int>
+    {
+        [Required]
+        //[Remote("IsAdvertisement_Exist", "RemotDataEx", AdditionalFields = "AdvertisementNo,AdvertisementID", ErrorMessage = "Entered Advertisement No Already exist in database. Please give unique Advertisement No.")]
+        [Remote(action: "Exist", controller: "Actions", AdditionalFields = "Id")]
+        public string Name { get; set; }
+        public byte IsActive { get; set; }
+
+    }
+}
---
+++ Libraries/Model/EntityConfiguration/ActionConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Libraries.Model.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Libraries.Model.EntityConfiguration
+{
+
+
+    public class ActionsConfiguration : IEntityTypeConfiguration<Actions>
+    {
+
+        public void Configure(EntityTypeBuilder<Actions> builder)
+        {
+            builder.ToTable("actions", "lms");
+
+            builder.HasIndex(e => e.Name)
+                .HasName("Name_UNIQUE")
+                .IsUnique();
+
+            builder.Property(e => e.Id).HasColumnType("int(11)");
+
+            builder.Property(e => e.CreatedBy).HasColumnType("int(11)");
+
+            builder.Property(e => e.CreatedDate).HasColumnType("date");
+
+            builder.Property(e => e.IsActive).HasColumnType("tinyint(4)");
+
+            builder.Property(e => e.ModifiedBy).HasColumnType("int(11)");
+
+            builder.Property(e => e.ModifiedDate).HasColumnType("date");
+
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            builder.Property(e => e.Icon)
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            builder.Property(e => e.Color)
+               .IsRequired()
+               .HasMaxLength(100)
+               .IsUnicode(false);
+        }
+    }
+}
