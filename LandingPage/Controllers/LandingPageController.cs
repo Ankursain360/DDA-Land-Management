5622ec3aa286762a85c8b485d58a98fa3e0dd0d9
angelnikitasingh2021@gmail.com <angelnikitasingh2021@gmail.com>
2020-10-07 14:03:01 +0530

    Add dynamic landing page

--- LandingPage/Controllers/LandingPageController.cs
+++ LandingPage/Controllers/LandingPageController.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Libraries.Model.Entity;
+using Libraries.Service.IApplicationService;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Notification;
+using Notification.Constants;
+using Notification.OptionEnums;
+using Microsoft.AspNetCore.Authorization;
+using Dto.Search;
 
-namespace DDAPropertyREG.Controllers
+namespace LandingPage.Controllers
 {
     public class LandingPageController : Controller
     {
-        public IActionResult Index()
+        private readonly IModuleService _moduleService;
+
+        public LandingPageController(IModuleService moduleService)
         {
-            return View();
+            _moduleService = moduleService;
         }
+        public async Task<IActionResult> Index()
+        {
+            var list = await _moduleService.GetAllModule();
+            return View(list);
+        }
+
         public IActionResult Create()
         {
             return View();
--- LandingPage/Startup.cs
+++ LandingPage/Startup.cs
@@ -1,29 +1,95 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Libraries.Model;
+using LandingPage.Infrastructure.Extensions;
+using System.IdentityModel.Tokens.Jwt;
+
+//using LandingPage.Filters;
 
 namespace LandingPage
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            HostEnvironment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment HostEnvironment { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<IFileProvider>(
+            new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
+            services.AddDbContext<DataContext>(a => a.UseMySQL(Configuration.GetSection("ConnectionString:Con").Value));
+
+            services.AddSingleton<ITempDataProvider, CookieTempDataProvider>();
+
+            services.Configure<CookieTempDataProviderOptions>(options =>
+            {
+                options.Cookie.Name = "MyTempDataCookie";
+            });
+            //services.AddMvc(option =>
+            //{
+            //    option.Filters.Add(new CustomExceptionHandlerFilter());
+            //});
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(20);
+                options.Cookie.IsEssential = true;
+            });
+            services.RegisterDependency();
+
+#if DEBUG
+            if (HostEnvironment.IsDevelopment())
+            {
+                services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            }
+            else
+            {
+                services.AddControllersWithViews();
+            }
+#endif
+
+            //JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+            //services.AddAuthentication(options =>
+            //{
+            //    options.DefaultScheme = "Cookies";
+            //    options.DefaultChallengeScheme = "oidc";
+            //})
+            //.AddCookie("Cookies")
+            //.AddOpenIdConnect("oidc", options =>
+            //{
+            //    options.Authority = "https://localhost:5001";
+
+            //    options.ClientId = "mvc";
+            //    options.ClientSecret = "secret";
+            //    options.ResponseType = "code";
+
+            //    options.Scope.Add("api1");
+
+            //    options.SaveTokens = true;
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +98,7 @@ namespace LandingPage
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
             }
             else
             {
@@ -41,16 +108,14 @@ namespace LandingPage
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
-
+            app.UseSession();
+            app.UseCookiePolicy();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
