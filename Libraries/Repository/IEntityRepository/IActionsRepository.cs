8a8b57b092e9564a4127e0919981580822287681
shalini rai r <shalini.rai@vedang.net>
2020-10-06 23:35:54 -0700

    error solved in action

---
+++ Libraries/Repository/IEntityRepository/IActionsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dto.Search;
+using Libraries.Model.Entity;
+using Libraries.Repository.Common;
+
+namespace Libraries.Repository.IEntityRepository
+{
+    public interface IActionsRepository : IGenericRepository<Actions>
+    {
+        Task<PagedResult<Actions>> GetPagedAction(ActionsSearchDto model);
+        Task<List<Actions>> GetAllActions();
+        Task<bool> Any(int id, string name);
+    }
+}
