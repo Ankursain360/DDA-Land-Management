366c110e3a9f4cd23a30027fe963eeb9dbdadb2c
Ashutosh <Ashutosh-Kumar.SHARMA@idemia.com>
2020-10-10 20:34:26 +0530

    clean code

--- Libraries/Dto/Master/ZoneDto.cs
+++ Libraries/Dto/Master/ZoneDto.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Dto.Common;
+using System;
 using System.Collections.Generic;
 
 namespace Dto.Master
 {
-    public class ZoneDto
+    public class ZoneDto : AuditableDto<int>
     {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public byte IsActive { get; set; }
     }
 }
