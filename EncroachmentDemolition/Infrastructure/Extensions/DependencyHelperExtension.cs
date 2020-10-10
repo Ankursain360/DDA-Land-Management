c7f406b620a6dfee2ce67f5feba770e746776b86
ishu rana <ishu.rana@vedang.net>
2020-10-10 14:45:57 +0530

    watch ward create edit view coding done

---
+++ EncroachmentDemolition/Infrastructure/Extensions/DependencyHelperExtension.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.DependencyInjection;
+using Libraries.Repository.IEntityRepository;
+using Libraries.Repository.EntityRepository;
+using Libraries.Service.IApplicationService;
+using Libraries.Service.ApplicationService;
+using Libraries.Repository.Common;
+using Model.Entity;
+using Microsoft.AspNetCore.Identity;
+
+namespace EncroachmentDemolition.Infrastructure.Extensions
+{
+    public static class DependencyHelperExtension
+    {
+        public static void RegisterDependency(this IServiceCollection services)
+        {
+            /* Common Dependencies */
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            /* Respository */
+            services.AddScoped<ICountryRepository, CountryRepository>();
+            services.AddScoped<IWatchandwardRepository, WatchandwardRepository>();
+
+
+            /* Application Services */
+            services.AddScoped<ICountryService, CountryService>();
+            services.AddScoped<IWatchandwardService,WatchandwardService>();
+        }
+    }
+}
