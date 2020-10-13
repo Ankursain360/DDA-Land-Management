ad7da1f7fe11cfe25d01a575fc4a87c228ec62e7
Ashutosh <Ashutosh-Kumar.SHARMA@idemia.com>
2020-10-13 16:38:03 +0530

    role master changes

---
+++ Libraries/Dto/Master/RoleDto.cs
@@ -0,0 +1,14 @@
+﻿using Dto.Common;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dto.Master
+{
+    public class RoleDto : AuditableDto<int>
+    {
+        [Required]
+        [Remote(action: "Exist", controller: "Role", AdditionalFields = "Id")]
+        public string Name { get; set; }
+        public short IsActive { get; set; }
+    }
+}
