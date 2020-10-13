d29aae861f0275a72be447bc5eaad99127052dfe
Renu Parashar <renu@vedang.net>
2020-10-12 18:02:16 -0700

    Changes related to all modules

---
+++ Libraries/Dto/Search/WorkflowLevelDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dto.Search
+{
+    public class WorkflowLevelDto
+    {
+        public string name { get; set; }
+    }
+}
