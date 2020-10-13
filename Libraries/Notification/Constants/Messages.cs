847b13b67750baba1d6f1f070f24e67976796914
shalini rai r <shalini.rai@vedang.net>
2020-10-13 00:58:42 -0700

    validation for report  watch and ward added

--- Libraries/Notification/Constants/Messages.cs
+++ Libraries/Notification/Constants/Messages.cs
@@ -7,6 +7,7 @@ namespace Notification.Constants
         public const string UpdateRecordSuccess = "Record updated successfully.";
         public const string DeleteSuccess = "Record deleted successfully.";
         public const string Error = "Unable to process the request.";
-        public const string RestoreSuccess = "Record restored successfully."; 
+        public const string RestoreSuccess = "Record restored successfully.";
+        public const string ValidationFailed = "Please fill all fields"; 
     }
 }
