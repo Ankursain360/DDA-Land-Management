766c2c5e9b0fac9b6e3a5e69cfeaeb7870504e78
ishu rana <ishu.rana@vedang.net>
2020-10-15 18:33:31 +0530

    multiple file save in watch ward done

---
+++ Libraries/Model/Entity/Watchandwardphotofiledetails.cs
@@ -0,0 +1,18 @@
+﻿using Libraries.Model.Common;
+using System;
+using System.Collections.Generic;
+
+
+namespace Libraries.Model.Entity
+{
+    public partial class Watchandwardphotofiledetails : AuditableEntity<int>
+    {
+       
+        public int WatchAndWardId { get; set; }
+        public string PhotoFilePath { get; set; }
+        public byte IsActive { get; set; }
+      
+
+        public virtual Watchandward WatchAndWard { get; set; }
+    }
+}
---
+++ Libraries/Model/EntityConfiguration/WatchandwardphotofiledetailsConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Libraries.Model.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.EntityConfiguration
+{
+   
+    class WatchandwardphotofiledetailsConfiguration : IEntityTypeConfiguration<Watchandwardphotofiledetails>
+    {
+
+        public void Configure(EntityTypeBuilder<Watchandwardphotofiledetails> builder)
+        {
+
+            builder.ToTable("watchandwardphotofiledetails", "lms");
+
+            builder.HasIndex(e => e.WatchAndWardId)
+                .HasName("FkWatchwardId_idx");
+
+            builder.Property(e => e.Id).HasColumnType("int(11)");
+
+            builder.Property(e => e.CreatedBy).HasColumnType("int(11)");
+
+            builder.Property(e => e.CreatedDate).HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            builder.Property(e => e.IsActive).HasColumnType("tinyint(4)");
+
+            builder.Property(e => e.ModifiedBy).HasColumnType("int(11)");
+
+            builder.Property(e => e.PhotoFilePath)
+                .IsRequired()
+                .HasMaxLength(1000)
+                .IsUnicode(false);
+
+            builder.Property(e => e.WatchAndWardId).HasColumnType("int(11)");
+
+            builder.HasOne(d => d.WatchAndWard)
+                .WithMany(p => p.Watchandwardphotofiledetails)
+                .HasForeignKey(d => d.WatchAndWardId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FkWatchwardId");
+
+        }
+    }
+}
