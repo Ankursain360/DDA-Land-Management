b1ef4ca851b4dc056bee7707fdebd37181f40161
angelnikitasingh2021@gmail.com <angelnikitasingh2021@gmail.com>
2020-10-21 17:03:01 +0530

    Add entity Demolition document

---
+++ Libraries/Model/Entity/Demolitiondocument.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Libraries.Model.Common;
+using Microsoft.AspNetCore.Mvc;
+using Model.Entity;
+
+namespace Libraries.Model.Entity
+{
+   public class Demolitiondocument : AuditableEntity<int>
+    {
+
+        [Required(ErrorMessage = "The Document Name field is required")]
+
+        public string DocumentName { get; set; }
+        [Required(ErrorMessage = "The Is Mandatory field is required")]
+
+        public string IsMandatory { get; set; }
+        public byte IsActive { get; set; }
+      
+    }
+}
