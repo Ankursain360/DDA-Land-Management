6ac2aebf573b2c01212839c393a5a66089ec9d93
Ashutosh <Ashutosh-Kumar.SHARMA@idemia.com>
2020-10-22 10:20:29 +0530

    commit Procedure call changes

--- Libraries/Dto/Search/InterestIndexDataDetails.cs
+++ Libraries/Dto/Search/InterestIndexDataDetails.cs
@@ -1,19 +1,16 @@
 ﻿using Dto.Common;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Dto.Search
 {
-   public class InterestIndexDataDetails : BaseSearchDto
+   public class InterestIndexDataDetails
     {
         public int Id { get; set; }
         public int PropertyId { get; set; }        
         public DateTime FromDate { get; set; }
         public DateTime ToDate { get; set; }
         public decimal Percentage { get; set; }
-        public byte IsActive { get; set; }
+        public System.SByte IsActive { get; set; }
         public string PropertyName { get; set; }
-
     }
 }
