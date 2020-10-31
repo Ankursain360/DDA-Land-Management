23e13341f86a42bef6b2cc7c3f60c0a2f9c5e484
shalini rai r <shalini.rai@vedang.net>
2020-10-31 07:20:52 -0700

    code added menu

--- Libraries/Model/Entity/Menu.cs
+++ Libraries/Model/Entity/Menu.cs
@@ -9,12 +9,17 @@ namespace Libraries.Model.Entity
 {
     public class Menu : AuditableEntity<int>
     {
-    
-       
+
+        [Required(ErrorMessage = "The Module Name field is required")]
         public int? ModuleId { get; set; }
+        [Required]
+        [Remote(action: "ExistName", controller: "Menu", AdditionalFields = "Id,ModuleId")]
         public string Name { get; set; }
+        [Required]
         public int? SortBy { get; set; }
+        [Required]
         public int? ParentMenuId { get; set; }
+        
         public byte? IsActive { get; set; }
         [NotMapped]
         public List<Module> modulelist { get; set; }
