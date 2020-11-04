0aba8d18d97fbd74ad7dfb6d7ffc889e4721be60
angelnikitasingh2021@gmail.com <angelnikitasingh2021@gmail.com>
2020-11-04 21:58:33 +0530

    Add Planning page

---
+++ LandTransfer/Controllers/PlanningController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Dto.Search;
+using Libraries.Model.Entity;
+using Libraries.Service.IApplicationService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Notification;
+using Notification.Constants;
+using Notification.OptionEnums;
+using Service.IApplicationService;
+using Utility.Helper;
+
+namespace LandTransfer.Controllers
+{
+    public class PlanningController : Controller
+    {
+
+        public IConfiguration _configuration;
+        public readonly IPropertyRegistrationService _propertyregistrationService;
+
+
+
+
+        public PlanningController(IPropertyRegistrationService propertyregistrationService, IConfiguration configuration)
+        {
+            _propertyregistrationService = propertyregistrationService;
+
+            _configuration = configuration;
+              }
+
+
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public async Task<IActionResult> ListPage(Propertyregistration propertyregistration)
+        {
+            propertyregistration.DepartmentList = await _propertyregistrationService.GetDepartmentDropDownList();
+
+            propertyregistration.ZoneList = await _propertyregistrationService.GetZoneDropDownList(propertyregistration.DepartmentId);
+           // propertyregistration.LocalityList = await _propertyregistrationService.GetLocalityDropDownList(propertyregistration.ZoneId);
+            propertyregistration.DivisionList = await _propertyregistrationService.GetDivisionDropDownList(propertyregistration.ZoneId);
+
+            return View(propertyregistration);
+        }
+
+
+    }
+}
