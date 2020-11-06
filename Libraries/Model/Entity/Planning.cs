b949f55a2ce5cee3479f53469b687d45df578783
Praveen kumar <70147586+vcspraveen@users.noreply.github.com>
2020-11-06 13:54:57 +0530

    Code For Planning

---
+++ Libraries/Model/Entity/Planning.cs
@@ -0,0 +1,18 @@
+﻿using Libraries.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Libraries.Model.Entity
+{
+    public class Planning :AuditableEntity<int>
+    {
+        public Planning()
+        {
+            PlanningProperties = new HashSet<PlanningProperties>();
+        }
+        public string Remarks { get; set; }
+        public byte? IsActive { get; set; }
+        public virtual ICollection<PlanningProperties> PlanningProperties { get; set; }
+    }
+}
---
+++ Libraries/Model/Entity/PlanningProperties.cs
@@ -0,0 +1,18 @@
+﻿using Libraries.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Libraries.Model.Entity
+{
+    public class PlanningProperties:AuditableEntity<int>
+    {
+        public int PlanningId { get; set; }
+        public int PropertyRegistrationId { get; set; }
+        public byte PropertyType { get; set; }
+        public byte? IsActive { get; set; }
+        public virtual Planning Planning { get; set; }
+        public virtual Propertyregistration PropertyRegistration { get; set; }
+
+    }
+}
