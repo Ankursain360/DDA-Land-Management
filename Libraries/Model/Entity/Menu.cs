27b0b7f446ff06e9540614fc09b3403046544780
Renu Parashar <renu@vedang.net>
2020-11-05 20:47:17 -0800

    menu changes

--- Libraries/Model/Entity/Menu.cs
+++ Libraries/Model/Entity/Menu.cs
@@ -9,6 +9,11 @@ namespace Libraries.Model.Entity
 {
     public class Menu : AuditableEntity<int>
     {
+        public Menu()
+        {
+            InverseParentMenu = new HashSet<Menu>();
+            Page = new HashSet<Page>();
+        }
 
         [Required(ErrorMessage = "The Module field is required")]
         public int? ModuleId { get; set; }
@@ -26,13 +31,12 @@ namespace Libraries.Model.Entity
         [NotMapped]
         public List<Menu> parentmenulist { get; set; }
 
-        //public virtual Menu menu { get; set; }
-
         [NotMapped]
         public List<Module> modulelist { get; set; }
 
         public virtual Module Module { get; set; }
-
+        public Menu ParentMenu { get; set; }
+        public ICollection<Menu> InverseParentMenu { get; set; }
         public ICollection<Page> Page { get; set; }
     }
 }
