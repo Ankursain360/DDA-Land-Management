29abb69cfb5dc815178b863fe5cb53f2bc00a483
Renu Parashar <renu@vedang.net>
2020-11-05 17:33:10 -0800

    table related changes

---
+++ Libraries/Model/EntityConfiguration/SubmenuactionrolemapConfiguration.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Libraries.Model.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Libraries.Model.EntityConfiguration
+{
+    public class SubmenuactionrolemapConfiguration : IEntityTypeConfiguration<Submenuactionrolemap>
+    {
+        public void Configure(EntityTypeBuilder<Submenuactionrolemap> builder)
+        {
+            builder.ToTable("submenuactionrolemap", "lms");
+
+            builder.HasIndex(e => e.ActionId)
+                .HasName("fk_menumap_action_id_idx");
+
+            builder.HasIndex(e => e.RoleId)
+                .HasName("fk_menumap_role_id_idx");
+
+            builder.HasIndex(e => e.SubMenuId)
+                .HasName("fk_menumap_submenu_id_idx");
+
+            builder.Property(e => e.Id).HasColumnType("int(11)");
+
+            builder.Property(e => e.ActionId).HasColumnType("int(11)");
+
+            builder.Property(e => e.CreatedBy).HasColumnType("int(11)");
+
+            builder.Property(e => e.CreatedDate).HasColumnType("date");
+
+            builder.Property(e => e.ModifiedBy).HasColumnType("int(11)");
+
+            builder.Property(e => e.ModifiedDate).HasColumnType("date");
+
+            builder.Property(e => e.RoleId).HasColumnType("int(11)");
+
+            builder.Property(e => e.SubMenuId).HasColumnType("int(11)");
+
+            builder.HasOne(d => d.Action)
+                .WithMany(p => p.Submenuactionrolemap)
+                .HasForeignKey(d => d.ActionId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_menumap_action_id");
+
+            builder.HasOne(d => d.Role)
+                .WithMany(p => p.Submenuactionrolemap)
+                .HasForeignKey(d => d.RoleId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_menumap_role_id");
+
+            builder.HasOne(d => d.SubMenu)
+                .WithMany(p => p.Submenuactionrolemap)
+                .HasForeignKey(d => d.SubMenuId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_menumap_submenu_id");
+        }
+    }
+}
