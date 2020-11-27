7093ba9f1560c726201d5cd6fd21d9c279b86337
ishu rana <ishu.rana@vedang.net>
2020-11-27 14:08:22 +0530

    side nav component added in inventory and encroachment module layout changed accordingly

--- EncroachmentDemolition/wwwroot/js/site.js
+++ EncroachmentDemolition/wwwroot/js/site.js
@@ -3,6 +3,15 @@
 
 // Write your JavaScript code.
 $(document).ready(function () {
+
+    $('#mobileMenu').on('click', function () {
+        $('#leftMenu').toggleClass('small');
+        $('#mobileMenu').toggleClass('open');
+        $('#rightSection').toggleClass('menu-close');
+        $('footer').toggleClass('menu-close');
+    });
+
+
     $(".ParameterActionListClass").attr("multiple", "");
     $("select[class!='form-control destroy']").select2({
         placeholder: "--Select--",
