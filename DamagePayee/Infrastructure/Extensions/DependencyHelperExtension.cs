65126dc89804296aec7bd3c76ece2160cf6038d5
ishu rana <ishu.rana@vedang.net>
2020-12-08 13:28:42 +0530

    layout damame payee updated

---
+++ DamagePayee/Infrastructure/Extensions/DependencyHelperExtension.cs
@@ -0,0 +1,41 @@
+﻿using DamagePayee.Helper;
+
+using Libraries.Repository.Common;
+using Libraries.Repository.EntityRepository;
+using Libraries.Repository.IEntityRepository;
+using Libraries.Service.ApplicationService;
+using Libraries.Service.IApplicationService;
+using Microsoft.Extensions.DependencyInjection;
+using Repository.EntityRepository;
+using Repository.IEntityRepository;
+using Service.ApplicationService;
+using Service.IApplicationService;
+
+namespace DamagePayee.Infrastructure.Extensions
+{
+    public static class DependencyHelperExtension
+    {
+        public static void RegisterDependency(this IServiceCollection services)
+        {
+            /* Common Dependencies */
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<ISiteContext, SiteContext>();
+
+            /* Respository */
+            services.AddScoped<ICountryRepository, CountryRepository>();
+           services.AddScoped<IUserProfileRepository, UserProfileRepository>();
+            services.AddScoped<IWorkflowTemplateRepository, WorkflowTemplateRepository>();
+            services.AddScoped<IPermissionsRepository, PermissionsRepository>();
+            services.AddScoped<IActionsRepository, ActionsRepository>();
+            services.AddScoped<IMenuRepository, MenuRepository>();
+           
+            /* Application Services */
+            services.AddScoped<ICountryService, CountryService>();
+             services.AddScoped<IUserProfileService, UserProfileService>();
+            services.AddScoped<IWorkflowTemplateService, WorkflowTemplateService>();
+            services.AddScoped<IPermissionsService, PermissionsService>();
+            services.AddScoped<IActionsService, ActionsService>();
+            services.AddScoped<IMenuService, MenuService>();
+            }
+    }
+}
