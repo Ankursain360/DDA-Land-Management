2e799b1662bdb54ae5164f2966f4f027734f5e2d
ishu rana <ishu.rana@vedang.net>
2020-12-09 13:50:54 +0530

    damage calculation report done

---
+++ DamagePayee/Controllers/DamageCalculatorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DamagePayee.Controllers
+{
+    public class DamageCalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
