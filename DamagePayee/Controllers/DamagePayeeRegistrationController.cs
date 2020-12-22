32833d5790f91d312eea1d2ddee916a79d8aeb69
shalini rai r <shalini.rai@vedang.net>
2020-12-22 07:50:50 -0800

    encryption decycrption in damage apyee registration

--- DamagePayee/Controllers/DamagePayeeRegistrationController.cs
+++ DamagePayee/Controllers/DamagePayeeRegistrationController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Notification;
 using Notification.Constants;
 using Notification.OptionEnums;
-
+using System.Web;
 using DamagePayee.Models;
 using System.Net;
 using System.IO;
@@ -15,6 +15,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
+using Utility.Helper;
+using AutoMapper.Configuration;
+using System.Text.Encodings.Web;
+using CCA.Util;
+using System.Linq;
+using Unidecode.NET;
 
 namespace DamagePayee.Controllers
 {
@@ -22,10 +29,10 @@ namespace DamagePayee.Controllers
 
     {
 
-        //private readonly lmsContext _context;
+        //private readonly IDataProtector protector;
         static string result = string.Empty;
         private readonly IHostingEnvironment _hostingEnvironment;
-
+        public Microsoft.Extensions.Configuration.IConfiguration _configuration;
         //public DamagePayeeRegistrationController(lmsContext context, IHostingEnvironment en)
         //{
         //    _context = context;
@@ -35,14 +42,23 @@ namespace DamagePayee.Controllers
 
         private readonly IDamagePayeeRegistrationService _damagePayeeRegistrationService;
 
-        public DamagePayeeRegistrationController(IDamagePayeeRegistrationService damagePayeeRegistrationService, IHostingEnvironment en)
+        public DamagePayeeRegistrationController(Microsoft.Extensions.Configuration.IConfiguration configuration,IDamagePayeeRegistrationService damagePayeeRegistrationService, IHostingEnvironment en)
         {
             _damagePayeeRegistrationService = damagePayeeRegistrationService;
-
+            _configuration = configuration;
             _hostingEnvironment = en;
         }
+
         public IActionResult Index()
         {
+            //var model = _damagePayeeRegistrationService.GetAllPayeeregistration()
+            //             .Select(e =>
+            //             {
+            //                  // Encrypt the ID value and store in EncryptedId property
+            //                  e.EncryptedId = protector.Protect(e.Id.ToString());
+            //                 return e;
+            //             });
+            ////return await _damagePayeeRegistrationService.GetAllPayeeregistration();
             return View();
         }
 
@@ -72,18 +88,20 @@ namespace DamagePayee.Controllers
 
                 if (ModelState.IsValid)
                 {
+                    var AesKey = _configuration.GetSection("EncryptionKey").Value.ToString();
                     payeeregistration.IsVerified = "F";
                     var result = await _damagePayeeRegistrationService.Create(payeeregistration);
 
                     if (result == true)
                     {
+                        EncryptionHelper encryptionHelper = new EncryptionHelper();
                         ViewBag.Message = Alert.Show(Messages.AddRecordSuccess, "", AlertType.Success);
 
                         var list = await _damagePayeeRegistrationService.GetAllPayeeregistration();
                         //At successfull completion send mail and sms
                         string DisplayName = payeeregistration.Name.ToString();
                         string EmailID = payeeregistration.EmailId.ToString();
-                        string Id = payeeregistration.Id.ToString();
+                        string Id = payeeregistration.Id.ToString().Unidecode();
                         string LoginName = payeeregistration.Name.ToString();
 
 
@@ -92,7 +110,8 @@ namespace DamagePayee.Controllers
                         string Action = "Dear " + LoginName + ",  You are succesfully registered with DDA Portal. For verify your email click  below link :-  " + uri;
                         var aburl = Request.GetDisplayUrl().Replace("Create", "RegistrationConfirmed");
                         string url = "https://www.google.com/search?q=yahoo+mail&rlz=1C1CHBF_enIN923IN923&oq=&aqs=chrome.1.69i59i450l5.24765349j0j15&sourceid=chrome&ie=UTF-8#=" + contactno + "&message= " + Action + " Thank you .&priority=11";
-                        string link = "<a target=\"_blank\" href=\"" + aburl + "?EmailID=" + EmailID + "&Id=" + Id + "\">Click Here</a>";
+                        
+                        string link = "<a target=\"_blank\" href=\"" + aburl + "?"+ encryptionHelper.EncryptString(AesKey, "EmailID") + "=" + encryptionHelper.EncryptString(AesKey, EmailID) + "&"+ encryptionHelper.EncryptString(AesKey, "Id") + "=" + encryptionHelper.EncryptString(AesKey, Id) + "\">Click Here</a>";
                         // Using WebRequest
                         WebRequest request = WebRequest.Create(url);
                         WebResponse response = request.GetResponse();
@@ -139,15 +158,17 @@ namespace DamagePayee.Controllers
 
 
         }
+       [HttpGet]
         public async Task<IActionResult> RegistrationConfirmed()
         {
-
+            EncryptionHelper encryptionHelper=new EncryptionHelper();
+            var AesKey = _configuration.GetSection("EncryptionKey").Value.ToString();
             Payeeregistration payeeregistration = new Payeeregistration();
-            var id2 = HttpContext.Request.Query["EmailID"];
-            var id3 = HttpContext.Request.Query["Id"];
-            var UniqueId = (Convert.ToInt32(id3));
+            var EncryptedEmailId = HttpContext.Request.Query[encryptionHelper.EncryptString(AesKey,"EmailID")];
+            var EncryptedId = HttpContext.Request.Query[encryptionHelper.EncryptString(AesKey, "Id")];
+            var UniqueId = Convert.ToInt32(encryptionHelper.DecryptString(AesKey,Convert.ToString(EncryptedId)).Replace(" ","+"));
+            var EmailId = encryptionHelper.DecryptString(AesKey, EncryptedEmailId).Replace(" ", "+");
             var id = Request.QueryString.Value;
-
             if (id == null)
             {
                 id = "0";
@@ -161,15 +182,22 @@ namespace DamagePayee.Controllers
                     payeeregistration.Id = UniqueId;
                     var result1 = await _damagePayeeRegistrationService.UpdateVerification(payeeregistration);
                     if (result1 == true)
-                        ViewBag.Message = Alert.Show(Messages.UpdateRecordSuccess, "", AlertType.Success);
+                        ViewBag.Message = Alert.Show(Messages.RegistrationConfirm, "", AlertType.Success);
 
-                    var list = await _damagePayeeRegistrationService.GetAllPayeeregistration();
-                    return View("Index", list);
+                    //var list = await _damagePayeeRegistrationService.GetAllPayeeregistration();
+                    // string decryptedId = protector.Unprotect(id);
+                    // int decryptedIntId = Convert.ToInt32(decryptedId);
+
+                    //var result2 = _damagePayeeRegistrationService.GetAllPayeeregistration(decryptedIntId);
+                    //return View("Index", list);
+                    ViewBag.Message = Alert.Show(Messages.UpdateRecordSuccess, "", AlertType.Success);
+                    // var result1 = await _damagepayeeregisterService.GetAllDamagepayeeregisterTemp();
+                    return View("RegistrationConfirmed");
                 }
                 else
                 {
                     ViewBag.Message = Alert.Show("User Not Found", "", AlertType.Error);
-                    return View("Index");
+                    return View("RegistrationConfirmed");
                 }
 
 
