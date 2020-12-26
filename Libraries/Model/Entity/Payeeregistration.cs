c58d6dbdedbfbbc5427702403eb22f1b748b8b21
shalini rai r <shalini.rai@vedang.net>
2020-12-26 07:10:46 -0800

    damage payeeregistration captcha

--- Libraries/Model/Entity/Payeeregistration.cs
+++ Libraries/Model/Entity/Payeeregistration.cs
@@ -10,16 +10,19 @@ namespace Libraries.Model.Entity
     public class Payeeregistration : AuditableEntity<int>
     {
         [Required]
-        //[Remote(action: "ExistName", controller: "DamagePayeeRegistration", AdditionalFields = "Id,Name")]
+        [Remote(action: "ExistName", controller: "DamagePayeeRegistration", AdditionalFields = "Id,Name")]
         public string Name { get; set; }
+        [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Please Enter a Valid 10 digit Mobile Number")]
         public string MobileNumber { get; set; }
         [Required(ErrorMessage = "The EmailId field is required")]
-        //[Remote(action: "Existemail", controller: "DamagePayeeRegistration", AdditionalFields = "Id,EmailId")]
+        [Remote(action: "Existemail", controller: "DamagePayeeRegistration", AdditionalFields = "Id,EmailId")]
         public string EmailId { get; set; }
         public string IsVerified { get; set; }
         public byte? IsActive { get; set; }
-        [NotMapped]
-        public string EncryptedId { get; set; }
+        
+        [Required]
+        [StringLength(4)]
+        public string CaptchaCode { get; set; }
 
     }
 }
