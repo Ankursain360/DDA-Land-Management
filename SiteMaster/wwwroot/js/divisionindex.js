fc3e0a71c779ba43b5a16920fb5d63918b4e4275
ishu rana <ishu.rana@vedang.net>
2021-01-04 21:25:14 +0530

    Division role structure search reset done

--- SiteMaster/wwwroot/js/divisionindex.js
+++ SiteMaster/wwwroot/js/divisionindex.js
@@ -5,23 +5,36 @@ $(document).ready(function () {
     GetDivision(currentPageNumber, currentPageSize);
 });
 
+$("#btnSearch").click(function () {
+    GetDivision(currentPageNumber, currentPageSize);
+});
+
+$("#btnReset").click(function () {
+    $('#txtName').val('');
+    $('#txtCode').val('')
+    GetUser(currentPageNumber, currentPageSize);
+});
+
 function GetDivision(pageNumber, pageSize) {
     var param = GetSearchParam(pageNumber, pageSize);
     HttpPost(`/division/List`, 'html', param, function (response) {
         $('#divDivisionTable').html("");
         $('#divDivisionTable').html(response);
     });
-    var widthPercentage = 100 / $('table').children('thead').children('tr').children('th').length;
-    $('table').children('thead').children('tr').children('th').css("width", widthPercentage.toString() + "%");
-    $('table').children('tbody').children('tr').children('th').css("width", widthPercentage.toString() + "%");
-    if ($('table >tbody >tr').length <= 1) {
-        GetDivision(1, $("#ddlPageSize option:selected").val());
-    }
+
+    //var widthPercentage = 100 / $('table').children('thead').children('tr').children('th').length;
+    //$('table').children('thead').children('tr').children('th').css("width", widthPercentage.toString() + "%");
+    //$('table').children('tbody').children('tr').children('th').css("width", widthPercentage.toString() + "%");
+
+    //if ($('table >tbody >tr').length <= 1) {
+    //    GetDivision(1, $("#ddlPageSize option:selected").val());
+    //}
 }
 
 function GetSearchParam(pageNumber, pageSize) {
     var model = {
-        name: "test",
+        name: $('#txtName').val(),
+        code: $('#txtCode').val(),
         pageSize: parseInt(pageSize),
         pageNumber: parseInt(pageNumber)
     }
