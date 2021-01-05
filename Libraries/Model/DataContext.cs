3d8f41ef8798d9a662d47a4139b90a8100ffed92
Renu Parashar <renu@vedang.net>
2021-01-04 20:17:08 -0800

    changes

--- Libraries/Model/DataContext.cs
+++ Libraries/Model/DataContext.cs
@@ -157,6 +157,13 @@ namespace Libraries.Model
         public virtual DbSet<Resratelisttypeb> Resratelisttypeb { get; set; }
         public virtual DbSet<Resratelisttypec> Resratelisttypec { get; set; }
         public virtual DbSet<Demandletter> Demandletter { get; set; }
+        public virtual DbSet<Comratelisttypea> Comratelisttypea { get; set; }
+        public virtual DbSet<Comratelisttypeb> Comratelisttypeb { get; set; }
+        public virtual DbSet<Comratelisttypec> Comratelisttypec { get; set; }
+        public virtual DbSet<Comsubencroacherstype> Comsubencroacherstype { get; set; }
+        public virtual DbSet<Ressubencroacherstype> Ressubencroacherstype { get; set; }
+        public virtual DbSet<Comencrochmenttype> Comencrochmenttype { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new SystemUserConfiguration());
@@ -276,7 +283,12 @@ namespace Libraries.Model
             modelBuilder.ApplyConfiguration(new ResRateListTypeAConfiguration());
             modelBuilder.ApplyConfiguration(new ResRateListTypeBConfiguration());
             modelBuilder.ApplyConfiguration(new ResRateListTypeCConfiguration());
-
+            modelBuilder.ApplyConfiguration(new ResSubEncroachersTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new ComRateListTypeAConfiguration());
+            modelBuilder.ApplyConfiguration(new ComRateListTypeBConfiguration());
+            modelBuilder.ApplyConfiguration(new ComRateListTypeCConfiguration());
+            modelBuilder.ApplyConfiguration(new ComSubEncroachersTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new ComEncrochmentTypeConfiguration());
             modelBuilder.ApplyConfiguration(new DemandletterConfiguration());
             base.OnModelCreating(modelBuilder);
         }
