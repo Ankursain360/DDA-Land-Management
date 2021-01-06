2ba495d756bf0abbfef5d548e523754a255d585c
Parul <Parul@vedang.net>
2021-01-06 13:24:14 +0530

    for sync

--- SiteMaster/wwwroot/js/landuseindex.js
+++ SiteMaster/wwwroot/js/landuseindex.js
@@ -1,13 +1,16 @@
 ﻿var currentPageNumber = 1;
 var currentPageSize = 10;
+var currentsortOrderAsc = 1;
+var currentsortOrderdesc = 2;
 
 $(document).ready(function () {
     GetDetails(currentPageNumber, currentPageSize);
 });
 
+
 function GetDetails(pageNumber, pageSize) {
     var param = GetSearchParam(pageNumber, pageSize);
-    HttpPost(`/LandUse/List`, 'html', param, function (response) {
+      HttpPost(`/LandUse/List`, 'html', param, function (response) {
         $('#divTable').html("");
         $('#divTable').html(response);
     });
@@ -16,22 +19,65 @@ function GetDetails(pageNumber, pageSize) {
     //    GetDetails(1, $("#ddlPageSize option:selected").val());
     //}
 }
+
+function GetLandUseOrderby(pageNumber, pageSize, order) {
+    var param = GetSearchParamOrderby(pageNumber, pageSize, order);
+    HttpPost(`/LandUse/List`, 'html', param, function (response) {
+        $('#divTable').html("");
+        $('#divTable').html(response);
+    });
+}
+
+
+function GetSearchParamOrderby(pageNumber, pageSize, order) {
+    var model = {
+        name: $('#txtName').val(),
+        sortBy: $("ddlViewBy").childern("option:selected").val();
+        sortOrder: parseInt(sortOrder),
+        pageSize: parseInt(pageSize),
+        pageNumber: parseInt(pageNumber)
+    }
+    debugger
+    return model;
+}
+
+
 $("#btnSearch").click(function () {
     GetDetails(currentPageNumber, currentPageSize);
 });
+
 $("#btnReset").click(function () {
     $("#txtName").val('');
     GetDetails(currentPageNumber, currentPageSize);
 });
 
+$("#btnAsc").click(function () {
+
+    // var e = document.getElementById("ddlViewBy");
+    //sortBy = e.value;
+    var sortBy = $("ddlViewBy").childern("option:selected").val();
+    $('#txtName').val('');
+    //document.getElementById("ddlViewBy").value;
+    //sortOrder = 'Asc';
+    if (sortBy !== "0") {
+
+        GetLandUseOrderby(currentpageNumber, currentpageSize, currentsortOrderAsc);
+    }
+    else { alert("Please select Sort by value."); }
+    //GetDetails(currentPageNumber, currentPageSize);
+});
+
+
+
+
+
 
 function GetSearchParam(pageNumber, pageSize) {
     var model = {
-        //name: "test",
-        name: $('#txtName').val(),
-        pageSize: pageSize,
+          name: $('#txtName').val(),
+          pageSize: pageSize,
         pageNumber: pageNumber
-    }
+            }
    // debugger
     return model;
 }
