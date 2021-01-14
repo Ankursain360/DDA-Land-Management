ddec71ea11854a01b38f0702249d3c89d1c74b2a
ishu rana <ishu.rana@vedang.net>
2021-01-14 18:20:50 +0530

    watch ward page bugs solved
    searching sorting done in watch ward

--- EncroachmentDemolition/wwwroot/js/watchandwardindex.js
+++ EncroachmentDemolition/wwwroot/js/watchandwardindex.js
@@ -1,26 +1,57 @@
 ﻿var currentPageNumber = 1;
 var currentPageSize = 10;
+var sortby = 1;//default Ascending 
 
 $(document).ready(function () {
-    GetWatchandward(currentPageNumber, currentPageSize);
+    GetWatchandward(currentPageNumber, currentPageSize, sortby);
 });
 
-function GetWatchandward(pageNumber, pageSize) {
-    var param = GetSearchParam(pageNumber, pageSize);
+function GetWatchandward(pageNumber, pageSize, order) {
+    var param = GetSearchParam(pageNumber, pageSize, order);
     HttpPost(`/WatchWard/List`, 'html', param, function (response) {
         $('#divWatchandwardTable').html("");
         $('#divWatchandwardTable').html(response);
     });
 }
 
-function GetSearchParam(pageNumber, pageSize) {
+function GetSearchParam(pageNumber, pageSize, sortOrder) {
     var model = {
-        name: "test",
-        pageSize: pageSize,
-        pageNumber: pageNumber
+        name: "WATCHWARD",
+        locality: $('#txtLocality').val(),
+        sortBy: $("#ddlSort").children("option:selected").val(),
+        sortOrder: parseInt(sortOrder),
+        pageSize: parseInt(pageSize),
+        pageNumber: parseInt(pageNumber)
     }
     return model;
 }
+
+
+$("#btnAscending").click(function () {
+    $("#btnDescending").removeClass("active");
+    $("#btnAscending").addClass("active");
+    sortby = 1;//for Ascending
+    GetWatchandward(currentPageNumber, currentPageSize, sortby);
+});
+
+
+$("#btnDescending").click(function () {
+    $("#btnAscending").removeClass("active");
+    $("#btnDescending").addClass("active");
+    sortby = 2;//for Descending
+    GetWatchandward(currentPageNumber, currentPageSize, sortby);
+});
+
+
+$("#btnSearch").click(function () {
+    GetWatchandward(currentPageNumber, currentPageSize, sortby);
+});
+
+$("#btnReset").click(function () {
+    $('#txtLocality').val('');
+    GetWatchandward(currentPageNumber, currentPageSize, sortby);
+
+});
 
 function onPaging(pageNo) {
     pageNo = parseInt(pageNo);
