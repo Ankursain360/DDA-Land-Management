79ca1a3ee16f3d21edede7c931c88334cbdf2e1d
Renu Parashar <renu@vedang.net>
2021-01-13 21:33:47 -0800

    sorting searching in relief report

--- DamagePayee/wwwroot/js/reliefreportindex.js
+++ DamagePayee/wwwroot/js/reliefreportindex.js
@@ -1,5 +1,8 @@
 ﻿var currentPageNumber = 1;
 var currentPageSize = 10;
+var currentSortOrderAscending = 1;
+var currentSortOrderDescending = 2;
+
 $(document).ready(function () {
 
     $("#btnGenerate").click(function () {
@@ -29,19 +32,15 @@ function GetDetails(pageNumber, pageSize) {
 
 function GetSearchParam(pageNumber, pageSize) {
     
-    var fileNoId = $('#FileNo option:selected').val();
-    var localityId = $('#LocalityId option:selected').val();
-    var fromDate = $('#FromDate').val();
-    var ToDate = $('#ToDate').val();
     var test = [];
 
     var model = {
         pageSize: parseInt(pageSize),
         pageNumber: parseInt(pageNumber),
-        FileNo: parseInt(fileNoId),
-        Locality: parseInt(localityId),
-        FromDate: (fromDate),
-        ToDate: (ToDate)
+        FileNo: parseInt(($('#FileNo option:selected').val())),
+        Locality: parseInt(($('#LocalityId option:selected').val())),
+        FromDate: (($('#FromDate').val())),
+        ToDate: (($('#ToDate').val()))
     }
     test.push(model);
     return model;
@@ -57,8 +56,52 @@ function onChangePageSize(pageSize) {
     GetDetails(parseInt(currentPageNumber), parseInt(pageSize));
     currentPageSize = pageSize;
 }
+function Descending() {
+    $("#btnAscending").removeClass("active");
+    $("#btnDescending").addClass("active");
+    var value = $("#ddlSort").children("option:selected").val();
+    $('#txtName').val('')
+    if (value !== "0") {
+        GetDetailsOrderby(currentPageNumber, currentPageSize, currentSortOrderDescending);
+    }
+    else {
+        alert('Please select SortBy Value');
+    }
+};
+function Ascending() {
+    $("#btnDescending").removeClass("active");
+    $("#btnAscending").addClass("active");
+    var value = $("#ddlSort").children("option:selected").val();
+    $('#txtName').val('')
 
+    if (value !== "0") {
 
+        GetDetailsOrderby(currentPageNumber, currentPageSize, currentSortOrderAscending);
+    }
+    else {
+        alert('Please select SortBy Value');
+    }
+};
+function GetDetailsOrderby(pageNumber, pageSize, order) {
+    var param = GetSearchParamaOrderby(pageNumber, pageSize, order);
+    HttpPost(`/ReliefReport/GetDetails`, 'html', param, function (response) {
+        $('#LoadReportView').html("");
+        $('#LoadReportView').html(response);
+    });
+}
+function GetSearchParamaOrderby(pageNumber, pageSize, sortOrder) {
+    var model = {
+        SortBy: $("#ddlSort").children("option:selected").val(),
+        SortOrder: parseInt(sortOrder),
+        pageSize: parseInt(pageSize),
+        pageNumber: parseInt(pageNumber),
+        FileNo: parseInt(($('#FileNo option:selected').val())),
+        Locality: parseInt(($('#LocalityId option:selected').val())),
+        FromDate: (($('#FromDate').val())),
+        ToDate: (($('#ToDate').val()))
+    }
+    return model;
+}
 function callSelect2() {
     $("select").select2({
         placeholder: "Select",
