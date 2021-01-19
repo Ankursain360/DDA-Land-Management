5eb3202fb03516c991da628fc78215c87bf343d5
shalini rai r <shalini.rai@vedang.net>
2021-01-18 23:55:02 -0800

    changes for sorting in encroachment demolition

--- Libraries/Dto/Search/InspectionEncroachmentregistrationSearchDto.cs
+++ Libraries/Dto/Search/InspectionEncroachmentregistrationSearchDto.cs
@@ -1,14 +1,16 @@
 ﻿using Dto.Common;
+using System;
+
 namespace Dto.Search
 {
-   public class EnchroachmentSearchDto : BaseSearchDto
+    public class InspectionEncroachmentregistrationSearchDto : BaseSearchDto
     {
         public string name { get; set; }
         public int departmentId { get; set; }
         public int zoneId { get; set; }
         public int divisionId { get; set; }
         public int localityId { get; set; }
-
-       
+        public DateTime fromDate { get; set; }
+        public DateTime toDate { get; set; }
     }
 }
