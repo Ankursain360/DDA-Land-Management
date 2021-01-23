149fe838fa83564adf94438d530eeaee05757a9c
Renu Parashar <renu@vedang.net>
2021-01-22 17:52:00 -0800

    appconstant change

--- Libraries/Utility/Helper/AppConstantsHelper.cs
+++ Libraries/Utility/Helper/AppConstantsHelper.cs
@@ -7,8 +7,8 @@ namespace Utility.Helper
     class AppConstantsHelper
     {
         public const string defaultPswd = "Pass123$";
-        public const string fromMail = "renuparasar7@gmail.com";
-        public const string fromMailPwd = "anitaparashar";
+        public const string fromMail = "vedangofficeserver@gmail.com";
+        public const string fromMailPwd = "Vedang@1234";
         public const string mailHost = "smtp.gmail.com";
         public const int port = 587;
     }
