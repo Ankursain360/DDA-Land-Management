40d8ca7b759d0c5a74116b6d364cf26c8555e6dc
shalini rai r <shalini.rai@vedang.net>
2021-01-27 07:16:19 -0800

    conflict resolved

--- Libraries/Dto/Search/PaymentTransactionReportListDataDto.cs
+++ Libraries/Dto/Search/PaymentTransactionReportListDataDto.cs
@@ -7,18 +7,26 @@ namespace Dto.Search
     {
         public int Id { get; set; }
         public string FileNo { get; set; }
-       
+
         public string LocalityName { get; set; }
-        public string PayeeName { get; set; }
         public string PropertyNo { get; set; }
+        public string PayeeName { get; set; }
+
+       
+        public string PaymentMode { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+
+       
         public Decimal AmountPaid { get; set; }
 
-        public DateTime CreatedDate { get; set; }
-        public string PaymentMode { get; set; }
+       
+      
         public Decimal PropertyArea { get; set; }
         public string DemandNo { get; set; }
         public DateTime DemandDate { get; set; }
         public string DemandAmount { get; set; }
         
+
     }
 }
