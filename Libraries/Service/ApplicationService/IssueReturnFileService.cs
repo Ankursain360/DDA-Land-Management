95d68024dae101aed4122a677cec8b0876a44068
ishu rana <ishu.rana@vedang.net>
2021-01-28 11:05:02 +0530

    issue file data coding done

--- Libraries/Service/ApplicationService/IssueReturnFileService.cs
+++ Libraries/Service/ApplicationService/IssueReturnFileService.cs
@@ -22,10 +22,33 @@ namespace Service.ApplicationService
             _unitOfWork = unitOfWork;
             _issueReturnFileRepository = issueReturnFileRepository;
         }
+        public async Task<List<Department>> GetAllDepartment()
+        {
+            List<Department> departmentList = await _issueReturnFileRepository.GetAllDepartment();
+            return departmentList;
+        }
+        public async Task<List<Branch>> GetAllBranch()
+        {
+            List<Branch> branchList = await _issueReturnFileRepository.GetAllBranch();
+            return branchList;
+        }
+        public async Task<List<Designation>> GetAllDesignation()
+        {
+            List<Designation> designationList = await _issueReturnFileRepository.GetAllDesignation();
+            return designationList;
+        }
+
         public async Task<List<Datastoragedetails>> GetFileNoList()
         {
             List<Datastoragedetails> fileNoList = await _issueReturnFileRepository.GetFileNoList();
             return fileNoList;
+        }
+        public async Task<bool> Create(Issuereturnfile model)
+        {
+        
+            model.CreatedDate = DateTime.Now;
+            _issueReturnFileRepository.Add(model);
+            return await _unitOfWork.CommitAsync() > 0;
         }
         public async Task<PagedResult<Datastoragedetails>> GetPagedIssueReturnFile(IssueReturnFileSearchDto model)
         {
