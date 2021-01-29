b53df18855b4032760a73811d216ff1a9cdb272c
Renu Parashar <renu@vedang.net>
2021-01-29 11:27:59 -0800

    new layout changes in document management system

---
+++ DocumentManagementSystem/Infrastructure/Extensions/DependencyHelperExtension.cs
@@ -0,0 +1,41 @@
+﻿using Libraries.Repository.Common;
+using Libraries.Repository.EntityRepository;
+using Libraries.Repository.IEntityRepository;
+using Libraries.Service.ApplicationService;
+using Libraries.Service.IApplicationService;
+using Microsoft.Extensions.DependencyInjection;
+using Repository.EntityRepository;
+using Repository.IEntityRepository;
+using Service.ApplicationService;
+using Service.IApplicationService;
+using DocumentManagementSystem.Helper;
+
+namespace DocumentManagementSystem.Infrastructure.Extensions
+{
+    public static class DependencyHelperExtension
+    {
+        public static void RegisterDependency(this IServiceCollection services)
+        {
+            /* Common Dependencies */
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<ISiteContext, SiteContext>();
+            /* Respository */
+            services.AddScoped<IModuleRepository, ModuleRepository>();
+            services.AddScoped<IWorkflowTemplateRepository, WorkflowTemplateRepository>();
+            services.AddScoped<IActionsRepository, ActionsRepository>();
+            services.AddScoped<IUserProfileRepository, UserProfileRepository>();
+            services.AddScoped<IMenuRepository, MenuRepository>();
+            services.AddScoped<IPermissionsRepository, PermissionsRepository>();
+
+
+            /* Application Services */
+            services.AddScoped<IModuleService, ModuleService>();
+            services.AddScoped<IWorkflowTemplateService, WorkflowTemplateService>();
+            services.AddScoped<IActionsService, ActionsService>();
+            services.AddScoped<IUserProfileService, UserProfileService>();
+            services.AddScoped<IMenuService, MenuService>();
+            services.AddScoped<IPermissionsService, PermissionsService>();
+
+        }
+    }
+}
