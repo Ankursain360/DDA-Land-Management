321b9470e4f222c19456031eb4b2f1f7e9a113fc
shalini rai r <shalini.rai@vedang.net>
2021-02-04 05:39:54 -0800

    Layout chnge in acquired alnd mamangement

--- AcquiredLandInformationManagement/Controllers/DashboardController.cs
+++ AcquiredLandInformationManagement/Controllers/DashboardController.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Notification;
+using Notification.Constants;
+using Notification.OptionEnums;
 
-namespace DamagePayee.Controllers
+namespace AcquiredLandInformationManagement.Controllers
 {
     public class DashboardController : Controller
     {
         public IActionResult Index()
         {
+            //ViewBag.Message = Alert.Show(Messages.AddRecordSuccess, "", AlertType.Success);
             return View();
         }
     }
