c4539ca6edb5e1b2c582154a99b4d0f1b086796b
ishu rana <ishu.rana@vedang.net>
2021-02-04 15:23:03 +0530

    monthly roaster searching sorting completed

--- Libraries/Dto/Search/MonthlyRoasterSearchDto.cs
+++ Libraries/Dto/Search/MonthlyRoasterSearchDto.cs
@@ -4,6 +4,11 @@ namespace Dto.Search
 {
     public class MonthlyRoasterSearchDto : BaseSearchDto
     {
-        public string Name { get; set; }
+        public string department { get; set; }
+        public string zone { get; set; }
+        public string division { get; set; }
+        public string locality { get; set; }
+        public string guard { get; set; }
+        public string year { get; set; }
     }
 }
