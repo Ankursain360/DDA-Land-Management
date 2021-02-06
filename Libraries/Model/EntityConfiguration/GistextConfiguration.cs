fed3f2983ac424ac8aef7242c3e4c764825b2635
Praveen kumar <70147586+vcspraveen@users.noreply.github.com>
2021-02-06 18:32:43 +0530

    GIS Model And Configuration

---
+++ Libraries/Model/EntityConfiguration/GistextConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Libraries.Model.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Libraries.Model.EntityConfiguration
+{
+    public class GistextConfiguration : IEntityTypeConfiguration<Gistext>
+    {
+        public void Configure(EntityTypeBuilder<Gistext> entity)
+        {
+            entity.ToTable("gistext", "lms");
+
+            entity.HasIndex(e => e.VillageId)
+                .HasName("GISText_idx");
+
+            entity.Property(e => e.Id).HasColumnType("int(11)");
+
+            entity.Property(e => e.CreatedBy).HasColumnType("int(11)");
+
+            entity.Property(e => e.CreatedDate).HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            entity.Property(e => e.IsActive).HasColumnType("tinyint(4)");
+
+            entity.Property(e => e.ModifiedBy).HasColumnType("int(11)");
+
+            entity.Property(e => e.Polygon).HasColumnType("longtext");
+
+            entity.Property(e => e.VillageId).HasColumnType("int(11)");
+
+            entity.Property(e => e.Xcoordinate)
+                .HasColumnName("XCoordinate")
+                .HasColumnType("decimal(12,8)");
+
+            entity.Property(e => e.Ycoordinate)
+                .HasColumnName("YCoordinate")
+                .HasColumnType("decimal(12,8)");
+
+            entity.HasOne(d => d.Village)
+                .WithMany(p => p.Gistext)
+                .HasForeignKey(d => d.VillageId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("GISText");
+        }
+    }
+}
