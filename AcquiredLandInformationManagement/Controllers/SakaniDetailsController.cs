63955cf44fee8e8513e1e250c60dd1805b32eae0
ishu rana <ishu.rana@vedang.net>
2021-02-17 18:13:59 +0530

    sakni page completed

--- AcquiredLandInformationManagement/Controllers/SakaniDetailsController.cs
+++ AcquiredLandInformationManagement/Controllers/SakaniDetailsController.cs
@@ -14,7 +14,7 @@ using Notification.OptionEnums;
 
 namespace AcquiredLandInformationManagement.Controllers
 {
-    public class SakaniDetailsController : Controller
+    public class SakaniDetailsController : BaseController
     {
         private readonly ISakanidetailService _sakanidetailService;
         public SakaniDetailsController(ISakanidetailService sakanidetailService)
@@ -22,69 +22,209 @@ namespace AcquiredLandInformationManagement.Controllers
             _sakanidetailService = sakanidetailService;
         }
 
-        public async Task<IActionResult> Index()
+        public  IActionResult Index()
         {
             return View();
         }
+
         [HttpPost]
         public async Task<PartialViewResult> List([FromBody] SakaniDetailsSearchDto model)
         {
-            var result = await _sakanidetailService.GetPagedSakanidetail(model);
+            var result = await _sakanidetailService.GetPagedSaknidetail(model);
             return PartialView("_List", result);
         }
 
         public async Task<IActionResult> Create()
         {
-            Saknidetails sakanidetail = new Saknidetails();
-            sakanidetail.IsActive = 1;
-          
-            return View(sakanidetail);
+            Saknidetails sakni = new Saknidetails();
+            sakni.IsActive = 1;
+            sakni.AcquiredlandvillageList = await _sakanidetailService.GetAllVillage();
+            sakni.KhasraList = await _sakanidetailService.GetAllKhasra(sakni.VillageId);
+            sakni.VillageId = 0;
+            return View(sakni);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Saknidetails sakanidetail)
+        public async Task<IActionResult> Create(Saknidetails sakni)
         {
-            try
-            {
-               
-                if (ModelState.IsValid)
-                {
-                    var result = await _sakanidetailService.Create(sakanidetail);
 
-                    if (result == true)
+            sakni.AcquiredlandvillageList = await _sakanidetailService.GetAllVillage();
+            sakni.KhasraList = await _sakanidetailService.GetAllKhasra(sakni.VillageId);
+            
+            if (ModelState.IsValid)
+            {
+                sakni.CreatedBy = SiteContext.UserId;
+                var result = await _sakanidetailService.Create(sakni);
+
+                if (result == true)
+                {
+                    //************ Save Owner  ************  
+                    if (sakni.OwnerName != null &&
+                        sakni.FatherName != null &&
+                        sakni.Address != null)
+
                     {
-                        ViewBag.Message = Alert.Show(Messages.AddRecordSuccess, "", AlertType.Success);
-                        var list = await _sakanidetailService.GetSakanidetail();
-                        return View("Index", list);
+                        if (sakni.FatherName.Count > 0 &&
+                            sakni.FatherName.Count > 0 &&
+                            sakni.Address.Count > 0
+                           )
+
+                        {
+                            List<Sakniowner> owner = new List<Sakniowner>();
+                            for (int i = 0; i < sakni.OwnerName.Count; i++)
+                            {
+                                owner.Add(new Sakniowner
+                                {
+                                    OwnerName = sakni.OwnerName.Count <= i ? string.Empty : sakni.OwnerName[i],
+                                    FatherName = sakni.FatherName.Count <= i ? string.Empty : sakni.FatherName[i],
+                                    Address = sakni.Address.Count <= i ? string.Empty : sakni.Address[i],
+                                    SakniDetailId = sakni.Id,
+                                    CreatedBy = SiteContext.UserId
+                                });
+                            }
+                            foreach (var item in owner)
+                            {
+                                result = await _sakanidetailService.SaveOwner(item);
+                            }
+                        }
                     }
-                    else
+
+
+                    //************ Save Lessee details  ************  
+
+                    if (sakni.LesseeName != null &&
+                        sakni.LFather != null &&
+                        sakni.LAddress != null &&
+                        sakni.LShare != null &&
+                        sakni.LMortgage != null
+                        )
+
                     {
-                        ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
-                        return View(sakanidetail);
+                        if (sakni.LesseeName.Count > 0 &&
+                            sakni.LFather.Count > 0 &&
+                            sakni.LAddress.Count > 0 &&
+                            sakni.LShare.Count > 0 &&
+                            sakni.LMortgage.Count > 0
+                           )
+
+                        {
+                            List<Saknilessee> lessee = new List<Saknilessee>();
+                            for (int i = 0; i < sakni.LesseeName.Count; i++)
+                            {
+                                lessee.Add(new Saknilessee
+                                {
+                                    LesseeName = sakni.LesseeName.Count <= i ? string.Empty : sakni.LesseeName[i],
+                                    FatherName = sakni.LFather.Count <= i ? string.Empty : sakni.LFather[i],
+                                    Address = sakni.LAddress.Count <= i ? string.Empty : sakni.LAddress[i],
+                                    LesseeShare = sakni.LShare.Count <= i ? string.Empty : sakni.LShare[i],
+                                    LesseeMortgage = sakni.LMortgage.Count <= i ? string.Empty : sakni.LMortgage[i],
+                                    SakniDetailId = sakni.Id,
+                                    CreatedBy = SiteContext.UserId
+                                });
+                            }
+                            foreach (var item in lessee)
+                            {
+                                result = await _sakanidetailService.Savelessee(item);
+                            }
+                        }
                     }
+
+                    //************ Save tenant  ************  
+
+                    if (sakni.TName != null &&
+                        sakni.TFatherName != null &&
+                        sakni.TAddress != null)
+
+                    {
+                        if (sakni.TName.Count > 0 &&
+                            sakni.TFatherName.Count > 0 &&
+                            sakni.TAddress.Count > 0
+                           )
+
+                        {
+                            List<Saknitenant> tenant = new List<Saknitenant>();
+                            for (int i = 0; i < sakni.TName.Count; i++)
+                            {
+                                tenant.Add(new Saknitenant
+                                {
+                                    TenantName = sakni.TName.Count <= i ? string.Empty : sakni.TName[i],
+                                    FatherName = sakni.TFatherName.Count <= i ? string.Empty : sakni.TFatherName[i],
+                                    Address = sakni.TAddress.Count <= i ? string.Empty : sakni.TAddress[i],
+                                    SakniDetailId = sakni.Id,
+                                    CreatedBy = SiteContext.UserId
+                                });
+                            }
+                            foreach (var item in tenant)
+                            {
+                                result = await _sakanidetailService.SaveTenant(item);
+                            }
+                        }
+                    }
+
+
+                    ViewBag.Message = Alert.Show(Messages.AddRecordSuccess, "", AlertType.Success);
+                    var list = await _sakanidetailService.GetAllSaknidetail();
+                    return View("Index", list);
+
                 }
                 else
                 {
-                    return View(sakanidetail);
+                    ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                    return View(sakni);
                 }
             }
-            catch (Exception ex)
+            else
             {
                 ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
-                return View(sakanidetail);
+                return View(sakni);
             }
+
         }
 
-
-
+        public async Task<JsonResult> GetDetailsOwner(int? Id)
+        {
+            Id = Id ?? 0;
+            var data = await _sakanidetailService.GetAllOwner(Convert.ToInt32(Id));
+            return Json(data.Select(x => new
+            {
+                x.Id,
+                x.OwnerName,
+                x.FatherName,
+                x.Address
+            }));
+        }
+        public async Task<JsonResult> GetDetailslesssee(int? Id)
+        {
+            Id = Id ?? 0;
+            var data = await _sakanidetailService.GetAllSaknilessee(Convert.ToInt32(Id));
+            return Json(data.Select(x => new
+            {
+                x.Id,
+                x.LesseeName,
+                x.FatherName,
+                x.Address,
+                x.LesseeShare,
+                x.LesseeMortgage
+            }));
+        }
+        public async Task<JsonResult> GetDetailsTenant(int? Id)
+        {
+            Id = Id ?? 0;
+            var data = await _sakanidetailService.GetAllTenant(Convert.ToInt32(Id));
+            return Json(data.Select(x => new
+            {
+                x.Id,
+                x.TenantName,
+                x.FatherName,
+                x.Address
+            }));
+        }
         public async Task<IActionResult> Edit(int id)
         {
             var Data = await _sakanidetailService.FetchSingleResult(id);
-
-
-           
-
+            Data.AcquiredlandvillageList = await _sakanidetailService.GetAllVillage();
+            Data.KhasraList = await _sakanidetailService.GetAllKhasra(Data.VillageId);
             if (Data == null)
             {
                 return NotFound();
@@ -94,42 +234,148 @@ namespace AcquiredLandInformationManagement.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, Saknidetails sakanidetail)
+        public async Task<IActionResult> Edit(int id, Saknidetails sakni)
         {
+
+            sakni.AcquiredlandvillageList = await _sakanidetailService.GetAllVillage();
+            sakni.KhasraList = await _sakanidetailService.GetAllKhasra(sakni.VillageId);
+
             if (ModelState.IsValid)
             {
-                try
+               
+                sakni.ModifiedBy = SiteContext.UserId;
+                var result = await _sakanidetailService.Update(id, sakni);
+
+                if (result == true)
                 {
-                    var result = await _sakanidetailService.Update(id, sakanidetail);
-                    if (result == true)
+                    //************ Save Owner  ************  
+                    if (sakni.OwnerName != null &&
+                        sakni.FatherName != null &&
+                        sakni.Address != null)
+
                     {
-                        ViewBag.Message = Alert.Show(Messages.UpdateRecordSuccess, "", AlertType.Success);
-                        var list = await _sakanidetailService.GetSakanidetail();
-                        return View("Index", list);
+                        if (sakni.FatherName.Count > 0 &&
+                            sakni.FatherName.Count > 0 &&
+                            sakni.Address.Count > 0
+                           )
+
+                        {
+                            List<Sakniowner> owner = new List<Sakniowner>();
+                            result = await _sakanidetailService.DeleteOwner(id);
+                            for (int i = 0; i < sakni.OwnerName.Count; i++)
+                            {
+                                owner.Add(new Sakniowner
+                                {
+                                    OwnerName = sakni.OwnerName.Count <= i ? string.Empty : sakni.OwnerName[i],
+                                    FatherName = sakni.FatherName.Count <= i ? string.Empty : sakni.FatherName[i],
+                                    Address = sakni.Address.Count <= i ? string.Empty : sakni.Address[i],
+                                    SakniDetailId = sakni.Id,
+                                    CreatedBy = SiteContext.UserId
+                                });
+                            }
+                            foreach (var item in owner)
+                            {
+                                result = await _sakanidetailService.SaveOwner(item);
+                            }
+                        }
                     }
-                    else
+
+
+                    //************ Save Lessee details  ************  
+
+                    if (sakni.LesseeName != null &&
+                        sakni.LFather != null &&
+                        sakni.LAddress != null &&
+                        sakni.LShare != null &&
+                        sakni.LMortgage != null
+                        )
+
                     {
-                        ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
-                        return View(sakanidetail);
+                        if (sakni.LesseeName.Count > 0 &&
+                            sakni.LFather.Count > 0 &&
+                            sakni.LAddress.Count > 0 &&
+                            sakni.LShare.Count > 0 &&
+                            sakni.LMortgage.Count > 0
+                           )
+
+                        {
+                            List<Saknilessee> lessee = new List<Saknilessee>();
+                            result = await _sakanidetailService.Deletelessee(id);
+                            for (int i = 0; i < sakni.LesseeName.Count; i++)
+                            {
+                                lessee.Add(new Saknilessee
+                                {
+                                    LesseeName = sakni.LesseeName.Count <= i ? string.Empty : sakni.LesseeName[i],
+                                    FatherName = sakni.LFather.Count <= i ? string.Empty : sakni.LFather[i],
+                                    Address = sakni.LAddress.Count <= i ? string.Empty : sakni.LAddress[i],
+                                    LesseeShare = sakni.LShare.Count <= i ? string.Empty : sakni.LShare[i],
+                                    LesseeMortgage = sakni.LMortgage.Count <= i ? string.Empty : sakni.LMortgage[i],
+                                    SakniDetailId = sakni.Id,
+                                    CreatedBy = SiteContext.UserId
+                                });
+                            }
+                            foreach (var item in lessee)
+                            {
+                                result = await _sakanidetailService.Savelessee(item);
+                            }
+                        }
                     }
+
+                    //************ Save tenant  ************  
+
+                    if (sakni.TName != null &&
+                        sakni.TFatherName != null &&
+                        sakni.TAddress != null)
+
+                    {
+                        if (sakni.TName.Count > 0 &&
+                            sakni.TFatherName.Count > 0 &&
+                            sakni.TAddress.Count > 0
+                           )
+
+                        {
+                            List<Saknitenant> tenant = new List<Saknitenant>();
+                            result = await _sakanidetailService.DeleteTenant(id);
+                            for (int i = 0; i < sakni.TName.Count; i++)
+                            {
+                                tenant.Add(new Saknitenant
+                                {
+                                    TenantName = sakni.TName.Count <= i ? string.Empty : sakni.TName[i],
+                                    FatherName = sakni.TFatherName.Count <= i ? string.Empty : sakni.TFatherName[i],
+                                    Address = sakni.TAddress.Count <= i ? string.Empty : sakni.TAddress[i],
+                                    SakniDetailId = sakni.Id,
+                                    CreatedBy = SiteContext.UserId
+                                });
+                            }
+                            foreach (var item in tenant)
+                            {
+                                result = await _sakanidetailService.SaveTenant(item);
+                            }
+                        }
+                    }
+
+
+                    ViewBag.Message = Alert.Show(Messages.AddRecordSuccess, "", AlertType.Success);
+                    var list = await _sakanidetailService.GetAllSaknidetail();
+                    return View("Index", list);
+
                 }
-                catch (Exception ex)
+                else
                 {
                     ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
-                    return View(sakanidetail);
+                    return View(sakni);
                 }
             }
             else
             {
-                return View(sakanidetail);
+                ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                return View(sakni);
             }
         }
 
         public async Task<IActionResult> Delete(int id)
         {
-            try
-            {
-
+           
                 var result = await _sakanidetailService.Delete(id);
                 if (result == true)
                 {
@@ -139,19 +385,17 @@ namespace AcquiredLandInformationManagement.Controllers
                 {
                     ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
                 }
-            }
-            catch (Exception ex)
-            {
-                ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
-            }
-            var list = await _sakanidetailService.GetSakanidetail();
-            return View("Index", list);
+            
+               var list = await _sakanidetailService.GetAllSaknidetail();
+               return View("Index", list);
         }
 
         public async Task<IActionResult> View(int id)
         {
             var Data = await _sakanidetailService.FetchSingleResult(id);
-            
+
+            Data.AcquiredlandvillageList = await _sakanidetailService.GetAllVillage();
+            Data.KhasraList = await _sakanidetailService.GetAllKhasra(Data.VillageId);
 
             if (Data == null)
             {
@@ -160,8 +404,11 @@ namespace AcquiredLandInformationManagement.Controllers
             return View(Data);
         }
 
-
-
-
+        [HttpGet]
+        public async Task<JsonResult> GetKhasraList(int? villageId)
+        {
+            villageId = villageId ?? 0;
+            return Json(await _sakanidetailService.GetAllKhasra(Convert.ToInt32(villageId)));
+        }
     }
 }
--- Libraries/Service/ApplicationService/SakanidetailService.cs
+++ Libraries/Service/ApplicationService/SakanidetailService.cs
@@ -25,16 +25,28 @@ namespace Libraries.Service.ApplicationService
             _unitOfWork = unitOfWork;
             _sakanidetailRepository = sakanidetailRepository;
         }
-
-
-        public async Task<PagedResult<Saknidetails>> GetPagedSakanidetail(SakaniDetailsSearchDto model)
+       
+        public async Task<PagedResult<Saknidetails>> GetPagedSaknidetail(SakaniDetailsSearchDto model)
         {
-            return await _sakanidetailRepository.GetPagedSakanidetail(model);
+            return await _sakanidetailRepository.GetPagedSaknidetail(model);
         }
 
 
+        public async Task<List<Acquiredlandvillage>> GetAllVillage()
+        {
+            List<Acquiredlandvillage> villageList = await _sakanidetailRepository.GetAllVillage();
+            return villageList;
+        }
+        public async Task<List<Khasra>> GetAllKhasra(int? villageId)
+        {
+            List<Khasra> khasraList = await _sakanidetailRepository.GetAllKhasra(villageId);
+            return khasraList;
+        }
+        public async Task<List<Saknidetails>> GetAllSaknidetail()
+        {
 
-
+            return await _sakanidetailRepository.GetAllSaknidetail();
+        }
 
         public async Task<bool> Delete(int id)
         {
@@ -51,77 +63,91 @@ namespace Libraries.Service.ApplicationService
             Saknidetails model = result.FirstOrDefault();
             return model;
         }
-
-
-        public async Task<List<Khewat>> GetAllKhewat()
-        {
-            List<Khewat> khewatList = await _sakanidetailRepository.GetAllKhewat();
-            return khewatList;
-        }
-        public async Task<List<Khasra>> BindKhasra()
-        {
-            List<Khasra> khasraList = await _sakanidetailRepository.BindKhasra();
-            return khasraList;
-        }
-        public async Task<List<Acquiredlandvillage>> GetAllVillage()
-        {
-            List<Acquiredlandvillage> villageList = await _sakanidetailRepository.GetAllVillage();
-            return villageList;
-        }
-
-
-        public async Task<List<Saknidetails>> GetSakanidetail()
-        {
-
-            return await _sakanidetailRepository.GetSakanidetail();
-        }
-
-
-
-        public async Task<List<Saknidetails>> GetSakanidetailUsingRepo()
-        {
-            return await _sakanidetailRepository.GetSakanidetail();
-        }
-
-        public async Task<bool> Update(int id, Saknidetails sakanidetail)
+        public async Task<bool> Update(int id, Saknidetails sakni)
         {
             var result = await _sakanidetailRepository.FindBy(a => a.Id == id);
             Saknidetails model = result.FirstOrDefault();
-           
-            model.VillageId = sakanidetail.VillageId;
-            model.KhasraId = sakanidetail.KhasraId;
-           
-            model.Location = sakanidetail.Location;
-           
-            model.Remarks = sakanidetail.Remarks;
-          
+
+            model.VillageId = sakni.VillageId;
+            model.KhasraId = sakni.KhasraId;
+            model.YearOfjamabandi = sakni.YearOfjamabandi;
+            model.NoOfKhewat = sakni.NoOfKhewat;
+            model.NoOfKhatauni = sakni.NoOfKhatauni;
+            model.Location = sakni.Location;
+            model.Mortgage = sakni.Mortgage;
+            model.RentAmount = sakni.RentAmount;
+            model.OldMutationNo = sakni.OldMutationNo;
+            model.Remarks = sakni.Remarks;
+            model.IsActive = sakni.IsActive;
             model.ModifiedDate = DateTime.Now;
-            model.ModifiedBy = 1;
+            model.ModifiedBy = sakni.ModifiedBy;
             _sakanidetailRepository.Edit(model);
             return await _unitOfWork.CommitAsync() > 0;
 
         }
 
-        public async Task<bool> Create(Saknidetails sakanidetail)
+        public async Task<bool> Create(Saknidetails sakni)
         {
-            sakanidetail.CreatedBy = 1;
-            sakanidetail.CreatedDate = DateTime.Now;
-            sakanidetail.IsActive = 1;
+            sakni.CreatedBy = sakni.CreatedBy;
+            sakni.CreatedDate = DateTime.Now;
+            sakni.IsActive = 1;
 
-            _sakanidetailRepository.Add(sakanidetail);
+            _sakanidetailRepository.Add(sakni);
             return await _unitOfWork.CommitAsync() > 0;
         }
 
+        //********* rpt ! Owner Details **********
+        public async Task<bool> SaveOwner(Sakniowner owner)
+        {
+            owner.CreatedBy = owner.CreatedBy;
+            owner.CreatedDate = DateTime.Now;
+            owner.IsActive = 1;
+            return await _sakanidetailRepository.SaveOwner(owner);
+        }
+        public async Task<List<Sakniowner>> GetAllOwner(int id)
+        {
+            return await _sakanidetailRepository.GetAllOwner(id);
+        }
+        public async Task<bool> DeleteOwner(int Id)
+        {
+            return await _sakanidetailRepository.DeleteOwner(Id);
+        }
 
+        //********* rpt ! Lessee Details **********
 
+        public async Task<bool> Savelessee(Saknilessee lessee)
+        {
+            lessee.CreatedBy = lessee.CreatedBy;
+            lessee.CreatedDate = DateTime.Now;
+            lessee.IsActive = 1;
+            return await _sakanidetailRepository.Savelessee(lessee);
+        }
+        public async Task<List<Saknilessee>> GetAllSaknilessee(int id)
+        {
+            return await _sakanidetailRepository.GetAllSaknilessee(id);
+        }
+        public async Task<bool> Deletelessee(int Id)
+        {
+            return await _sakanidetailRepository.Deletelessee(Id);
+        }
 
+        //********* rpt ! Tenant Details **********
 
-
-
-
-
-
-
+        public async Task<bool> SaveTenant(Saknitenant tenant)
+        {
+            tenant.CreatedBy = tenant.CreatedBy;
+            tenant.CreatedDate = DateTime.Now;
+            tenant.IsActive = 1;
+            return await _sakanidetailRepository.SaveTenant(tenant);
+        }
+        public async Task<List<Saknitenant>> GetAllTenant(int id)
+        {
+            return await _sakanidetailRepository.GetAllTenant(id);
+        }
+        public async Task<bool> DeleteTenant(int Id)
+        {
+            return await _sakanidetailRepository.DeleteTenant(Id);
+        }
 
 
 
