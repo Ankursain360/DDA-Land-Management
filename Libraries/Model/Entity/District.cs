03bd89738cee755d2b175b6c4ced8d0f85cea4f5
Parul <Parul@vedang.net>
2021-02-20 13:04:27 +0530

    For Sync

--- Libraries/Model/Entity/District.cs
+++ Libraries/Model/Entity/District.cs
@@ -15,6 +15,7 @@ namespace Libraries.Model.Entity
             Acquiredlandvillage = new HashSet<Acquiredlandvillage>();
             Damagepayeeregister = new HashSet<Damagepayeeregister>();
             Userprofile = new HashSet<Userprofile>();
+            Newlandvillage = new HashSet<Newlandvillage>();
           
         }
         [Required(ErrorMessage = "District name is Mandatory")]
@@ -29,7 +30,8 @@ namespace Libraries.Model.Entity
         public virtual ICollection<Userprofile> Userprofile { get; set; }
         public ICollection<Acquiredlandvillage> Acquiredlandvillage { get; set; }
         public ICollection<Damagepayeeregister> Damagepayeeregister { get; set; }
-       
+        public ICollection<Newlandvillage> Newlandvillage { get; set; }
+
 
     }
 }
