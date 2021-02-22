2d99a1b312d4d75d43d5f041fb43b84a08f1e22f
ishu rana <ishu.rana@vedang.net>
2021-02-22 21:44:24 +0530

    new land us bugs solved

--- Libraries/Model/Entity/Newlandus6plot.cs
+++ Libraries/Model/Entity/Newlandus6plot.cs
@@ -12,18 +12,22 @@ namespace Libraries.Model.Entity
         public int NotificationId { get; set; }
         [Required(ErrorMessage = " Village name is mandatory")]
         public int VillageId { get; set; }
-        [Required(ErrorMessage = " Khasra name is mandatory")]
+        [Required(ErrorMessage = " Khasra no is mandatory")]
         public int KhasraId { get; set; }
+        [Required(ErrorMessage = " Bigha is mandatory")]
         [RegularExpression(@"((\d+)((\.\d{1,3})?))$", ErrorMessage = "Please enter valid integer or decimal number with 3 decimal places.")]
         [Range(0, 9999999999999999.99, ErrorMessage = "Invalid Total Area; Max 18 digits")]
         public decimal? Bigha { get; set; }
+        [Required(ErrorMessage = " Biswa is mandatory")]
         [RegularExpression(@"((\d+)((\.\d{1,3})?))$", ErrorMessage = "Please enter valid integer or decimal number with 3 decimal places.")]
         [Range(0, 9999999999999999.99, ErrorMessage = "Invalid Total Area; Max 18 digits")]
         public decimal? Biswa { get; set; }
+        [Required(ErrorMessage = " Biswanshi is mandatory")]
         [RegularExpression(@"((\d+)((\.\d{1,3})?))$", ErrorMessage = "Please enter valid integer or decimal number with 3 decimal places.")]
         [Range(0, 9999999999999999.99, ErrorMessage = "Invalid Total Area; Max 18 digits")]
         public decimal? Biswanshi { get; set; }
         public string Remarks { get; set; }
+        [Required(ErrorMessage = " Status is mandatory")]
         public byte? IsActive { get; set; }
        
 
