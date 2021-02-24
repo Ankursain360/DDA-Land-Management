8b6d407ffc3ab0403a7d0d78533bf4f48e7ef506
ishu rana <ishu.rana@vedang.net>
2021-02-24 22:15:50 +0530

    new land us22 page competed

--- NewLandAcquisition/Infrastructure/Extensions/DependencyHelperExtension.cs
+++ NewLandAcquisition/Infrastructure/Extensions/DependencyHelperExtension.cs
@@ -42,7 +42,7 @@ namespace NewLandAcquisition.Infrastructure.Extensions
             services.AddScoped<INewlandus6plotRepository, Newlandus6plotRepository>();
             services.AddScoped<INewlandus17plotRepository, Newlandus17plotRepository>();
             services.AddScoped<INewlandjointsurveyRepository,NewlandjointsurveyRepository>();
-
+            services.AddScoped<INewlandus22plotRepository, Newlandus22plotRepository>();
 
 
             /* Application Services */
@@ -70,7 +70,7 @@ namespace NewLandAcquisition.Infrastructure.Extensions
             services.AddScoped<INewlandus6plotService, Newlandus6plotService>();
             services.AddScoped<INewlandus17plotService, Newlandus17plotService>();
             services.AddScoped<INewlandjointsurveyService, NewlandjointsurveyService>();
-
+            services.AddScoped<INewlandus22plotService, Newlandus22plotService>();
         }
     }
 }
