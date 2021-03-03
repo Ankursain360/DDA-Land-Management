0c59f126c3f2769866ee0069c8f81ecc13ee1e37
Nikhil Sharma <nikhil.sharma3100@gmail.com>
2021-03-03 16:15:20 +0530

    Repeater Code Added in Joint Survey

--- NewLandAcquisition/wwwroot/js/NewlandJointSurveyCreate.js
+++ NewLandAcquisition/wwwroot/js/NewlandJointSurveyCreate.js
@@ -35,3 +35,32 @@ $("#KhasraId").change(function () {
 
     }
 });
+
+$(document).delegate('a.add-record', 'click', function (e) {
+    debugger
+    if ($("#tbl_posts #add #DocumentName").val() != '' && $("#tbl_posts #add #Document").val() !='') {
+       
+        e.preventDefault();
+        var content = jQuery('#tbl_posts #add tr'),
+            size = jQuery('#tbl_posts >tbody >tr').length,
+            element = null,
+            element = content.clone();
+        element.attr('id', 'rec-' + size);
+        element.find('.delete-record').attr('data-id', size);
+        element.appendTo('#tbl_posts_body');       
+        element.find('.sn').html(size);
+        $("#tbl_posts #add .sn").text($('#tbl_posts >tbody >tr').length);
+        $("#tbl_posts #add .add").remove();      
+        $("#tbl_posts #tbl_posts_body .floating-label-field").attr("readonly", true);
+        element.find(".add-record").hide();
+        element.find(".delete-record").show();
+        debugger
+        /*$("#tbl_posts #add .form-control").val('');*/
+        $("#tbl_posts #add .floating-label-field").val('');
+    }
+    else {
+        alert('Please fill record before add new record ');
+    }
+});
+
+
