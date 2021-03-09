56d28ed8b1a53e30a5b287d5be5edfcc7df5e84a
Parul <Parul@vedang.net>
2021-03-09 18:56:19 +0530

    for sync

--- LeaseDetails/Infrastructure/Extensions/DependencyHelperExtension.cs
+++ LeaseDetails/Infrastructure/Extensions/DependencyHelperExtension.cs
@@ -28,7 +28,6 @@ namespace LeaseDetails.Infrastructure.Extensions
             services.AddScoped<IWorkflowTemplateRepository, WorkflowTemplateRepository>();
             services.AddScoped<IUserProfileRepository, UserProfileRepository>();
             services.AddScoped<IApprovalProccessRepository, ApprovalProccessRepository>();
-            services.AddScoped<IPremiumrateRepository, PremiumrateRepository>();
 
 
             /* Application Services */
@@ -41,8 +40,8 @@ namespace LeaseDetails.Infrastructure.Extensions
             services.AddScoped<IWorkflowTemplateService, WorkflowTemplateService>();
             services.AddScoped<IActionsService, ActionsService>();
             services.AddScoped<IUserProfileService, UserProfileService>();
-            services.AddScoped<IPremiumrateService, PremiumrateService>();
-
+           
+          
 
         }
     }
