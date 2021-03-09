024543014a21a319fd3c3b10f4d0a695f0e8749d
Parul <Parul@vedang.net>
2021-03-09 11:58:11 +0530

    FOR SYNC

---
+++ Libraries/Model/Entity/Goldrate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Libraries.Model.Common;
+
+namespace Libraries.Model.Entity
+{
+    public class Goldrate : AuditableEntity<int>
+    {
+        public int PropertyTypeId { get; set; }
+        public decimal GoldRate1 { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public byte? IsActive { get; set; }
+       
+
+        public PropertyType PropertyType { get; set; }
+    }
+}
---
+++ Libraries/Model/EntityConfiguration/GoldrateConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Libraries.Model.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.EntityConfiguration
+{
+    public class GoldrateConfiguration : IEntityTypeConfiguration<Goldrate>
+    {
+        public void Configure(EntityTypeBuilder<Goldrate> builder)
+        {
+            builder.ToTable("goldrate", "lms");
+
+            builder.HasIndex(e => e.PropertyTypeId)
+                .HasName("fkpropertytype_idx");
+
+            builder.Property(e => e.Id).HasColumnType("int(11)");
+
+            builder.Property(e => e.CreatedBy).HasColumnType("int(11)");
+
+            builder.Property(e => e.CreatedDate).HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            builder.Property(e => e.FromDate).HasColumnType("date");
+
+            builder.Property(e => e.GoldRate1)
+                .HasColumnName("GoldRate")
+                .HasColumnType("decimal(18,3)");
+
+            builder.Property(e => e.IsActive).HasColumnType("tinyint(4)");
+
+            builder.Property(e => e.ModifiedBy).HasColumnType("int(11)");
+
+            builder.Property(e => e.PropertyTypeId).HasColumnType("int(11)");
+
+            builder.Property(e => e.ToDate).HasColumnType("date");
+
+            builder.HasOne(d => d.PropertyType)
+                .WithMany(p => p.Goldrate)
+                .HasForeignKey(d => d.PropertyTypeId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_propertytype");
+        }
+        }
+    }
