a54113726c5468c65421d418b6a89949bfc01473
nikita <nikita@vedang.net>
2021-03-11 18:34:56 +0530

    bug solved in new land acquistion

--- NewLandAcquisition/wwwroot/js/possessiondetailscreate.js
+++ NewLandAcquisition/wwwroot/js/possessiondetailscreate.js
@@ -2,7 +2,7 @@
  
     HttpGet(`/NewlandPossesionDetails/GetKhasraList/?villageId=${id}`, 'json', function (response) {
       
-        var html = '<option value="">Select</option>';
+        var html = '<option value=""> --Select--</option>';
         for (var i = 0; i < response.length; i++) {
             html = html + '<option value=' + response[i].id + '>' + response[i].name + '</option>';
         }
