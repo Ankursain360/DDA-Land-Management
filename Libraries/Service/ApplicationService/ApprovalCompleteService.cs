37f9dd566c6580515e0bdfcde6c68a96f7c0b29c
nikita <nikita@vedang.net>
2021-03-16 21:45:01 +0530

    Add Approval Process in Site master

---
+++ Libraries/Service/ApplicationService/ApprovalCompleteService.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Dto.Master;
+using Dto.Search;
+using Libraries.Model.Entity;
+using Libraries.Repository.Common;
+using Libraries.Service.Common;
+using Microsoft.AspNetCore.Identity;
+using Model.Entity;
+using Repository.IEntityRepository;
+using Service.IApplicationService;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+using System.Linq;
+
+namespace Service.ApplicationService
+{
+   public class ApprovalCompleteService : EntityService<Approvalproccess>, IApprovalCompleteService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IApprovalCompleteRepository _userrightRepository;
+
+        public ApprovalCompleteService(IUnitOfWork unitOfWork, IApprovalCompleteRepository userrightRepository)
+ : base(unitOfWork, userrightRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _userrightRepository = userrightRepository;
+        }
+
+
+        public async Task<List<ApprovalCompleteListDataDto>> GetApprovalCompleteModule(ApprovalCompleteSearchDto model)
+        {
+            return await _userrightRepository.GetApprovalCompleteModule(model);
+        }
+
+        public async Task<List<ApprovalCompleteListDataDto>> BindModuleName()
+        {
+            return await _userrightRepository.BindModuleName();
+        }
+
+    }
+}
--- SiteMaster/Controllers/ApprovalProcessController.cs
+++ SiteMaster/Controllers/ApprovalProcessController.cs
@@ -1,15 +1,73 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Libraries.Model.Entity;
+using Libraries.Service.IApplicationService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Notification;
+using Notification.Constants;
+using Notification.OptionEnums;
+using Dto.Search;
+using Microsoft.Extensions.Configuration;
+using Utility.Helper;
+using Dto.Common;
+using SiteMaster.Filters;
+using Service.IApplicationService;
+
+
+using Core.Enum;
+
 
 namespace SiteMaster.Controllers
 {
-    public class ApprovalProcessController : Controller
+   
+    public class ApprovalProcessController : BaseController
     {
+        public IConfiguration _configuration;
+        private readonly IApprovalCompleteService _approvalCompleteService;
+
+
+        public ApprovalProcessController(IApprovalCompleteService approvalcompleteService, IConfiguration configuration)
+        {
+            _approvalCompleteService = approvalcompleteService;
+          
+           
+        }
+
+
+        [HttpPost]
+        public async Task<PartialViewResult> GetDetails([FromBody] ApprovalCompleteSearchDto report)
+        {
+           
+            int  UserId = SiteContext.UserId;
+            report.userid = UserId;
+            var result = await _approvalCompleteService.GetApprovalCompleteModule(report);
+            if (result != null)
+            {
+                return PartialView("_List", result);
+            }
+            else
+            {
+                ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                return PartialView();
+            }
+        }
+
+
+
+    
+
+
+
+      
+
+
         public IActionResult Index()
         {
+            // int a = SiteContext.UserId;
             return View();
         }
     }
