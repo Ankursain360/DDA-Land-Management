155bd793a94c3175033c2906c34f91c85399a76e
Renu Parashar <renu@vedang.net>
2021-03-18 15:37:02 +0530

    Services and repository added for ProceedingEvictionLetter

---
+++ LeaseDetails/Controllers/ProceedingEvictionLetterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Libraries.Model.Entity;
+using Libraries.Service.IApplicationService;
+using LeaseDetails.Models;
+using Notification;
+using Notification.Constants;
+using Notification.OptionEnums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Dto.Search;
+using LeaseDetails.Filters;
+using Core.Enum;
+using Utility.Helper;
+
+namespace LeaseDetails.Controllers
+{
+    public class ProceedingEvictionLetterController : BaseController
+    {
+
+        private readonly IProceedingEvictionLetterService _proceedingEvictionLetterService;
+
+        public ProceedingEvictionLetterController(IProceedingEvictionLetterService proceedingEvictionLetterService)
+        {
+            _proceedingEvictionLetterService = proceedingEvictionLetterService;
+        }
+    }
+}
---
+++ Libraries/Repository/EntityRepository/ProceedingEvictionLetterRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dto.Search;
+using Libraries.Model;
+using Libraries.Model.Entity;
+using Libraries.Repository.Common;
+using Libraries.Repository.IEntityRepository;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Libraries.Repository.EntityRepository
+{
+    public class ProceedingEvictionLetterRepository : GenericRepository<Leaseapplication>, IProceedingEvictionLetterRepository
+    {
+
+        public ProceedingEvictionLetterRepository(DataContext dbContext) : base(dbContext)
+        {
+
+        }
+
+    }
+
+
+}
