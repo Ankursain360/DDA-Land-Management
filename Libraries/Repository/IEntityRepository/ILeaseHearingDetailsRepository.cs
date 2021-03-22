5d064f462f04f401e7d9418133067217e5680d53
Renu Parashar <renu@vedang.net>
2021-03-22 13:19:00 +0530

    repository and services added for the hearing details page

---
+++ Libraries/Repository/IEntityRepository/ILeaseHearingDetailsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dto.Master;
+using Dto.Search;
+using Libraries.Model.Entity;
+using Libraries.Repository.Common;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Libraries.Repository.IEntityRepository
+{
+    public interface ILeaseHearingDetailsRepository : IGenericRepository<Requestforproceeding>
+    {
+    }
+}
