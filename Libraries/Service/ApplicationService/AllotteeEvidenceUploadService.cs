24800a4cb3d4ce3480b6c3ed49f325911ef07ad7
Renu Parashar <renu@vedang.net>
2021-03-26 17:37:56 +0530

    Services and repository added for allotteeevidenceupload

---
+++ Libraries/Service/ApplicationService/AllotteeEvidenceUploadService.cs
@@ -0,0 +1,73 @@
+﻿using Libraries.Model.Entity;
+using Libraries.Repository.Common;
+using Libraries.Service.Common;
+using Libraries.Service.IApplicationService;
+using Libraries.Repository.IEntityRepository;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Dto.Search;
+using Dto.Master;
+using AutoMapper;
+
+namespace Libraries.Service.ApplicationService
+{
+
+    public class AllotteeEvidenceUploadService : EntityService<Leasenoticegeneration>, IAllotteeEvidenceUploadService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly INoticeGenerationRepository _noticeGenerationRepository;
+        private readonly IMapper _mapper;
+        public AllotteeEvidenceUploadService(IUnitOfWork unitOfWork,
+            INoticeGenerationRepository noticeGenerationRepository,
+            IMapper mapper)
+        : base(unitOfWork, noticeGenerationRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _noticeGenerationRepository = noticeGenerationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Leasenoticegeneration> FetchNoticeGenerationDetails(int id)
+        {
+            return await _noticeGenerationRepository.FetchNoticeGenerationDetails(id);
+        }
+
+        public async Task<List<Leasenoticegeneration>> GetNoticeHistoryDetails(int id)
+        {
+            return await _noticeGenerationRepository.GetNoticeHistoryDetails(id);
+        }
+
+        public async Task<PagedResult<Requestforproceeding>> GetPagedRequestLetterDetails(LeaseHearingDetailsSearchDto model)
+        {
+            return await _noticeGenerationRepository.GetPagedRequestLetterDetails(model);
+        }
+
+        public async Task<bool> Update(int id, Leasenoticegeneration leasenoticegeneration)
+        {
+            var result = await _noticeGenerationRepository.FindBy(a => a.Id == id);
+            Leasenoticegeneration model = result.FirstOrDefault();
+            model.RequestProceedingId = leasenoticegeneration.RequestProceedingId;
+            if (leasenoticegeneration.GenerateUpload == 0)
+            {
+                model.MeetingPlace = leasenoticegeneration.MeetingPlace;
+                model.MeetingDate = leasenoticegeneration.MeetingDate;
+                model.MeetingTime = leasenoticegeneration.MeetingTime;
+            }
+            else
+                model.NoticeFileName = leasenoticegeneration.NoticeFileName;
+            model.ModifiedDate = DateTime.Now;
+            model.ModifiedBy = leasenoticegeneration.ModifiedBy;
+            _noticeGenerationRepository.Edit(model);
+            return await _unitOfWork.CommitAsync() > 0;
+        }
+
+        public async Task<bool> Create(Leasenoticegeneration leasenoticegeneration)
+        {
+            leasenoticegeneration.CreatedDate = DateTime.Now;
+            _noticeGenerationRepository.Add(leasenoticegeneration);
+            return await _unitOfWork.CommitAsync() > 0;
+        }
+    }
+}
---
+++ Libraries/Service/IApplicationService/IAllotteeEvidenceUploadService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dto.Master;
+using Dto.Search;
+using Libraries.Model.Entity;
+using Libraries.Repository.Common;
+using Libraries.Service.Common;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Libraries.Service.IApplicationService
+{
+    public interface IAllotteeEvidenceUploadService : IEntityService<Leasenoticegeneration>
+    {
+        Task<PagedResult<Requestforproceeding>> GetPagedRequestLetterDetails(LeaseHearingDetailsSearchDto model);
+        Task<Leasenoticegeneration> FetchNoticeGenerationDetails(int id);
+        Task<List<Leasenoticegeneration>> GetNoticeHistoryDetails(int id);
+        Task<bool> Create(Leasenoticegeneration leasenoticegeneration);
+        Task<bool> Update(int id, Leasenoticegeneration leasenoticegeneration);
+    }
+}
