472551a4f0f919065757985067c366f4ce20c836
ishu.rana@vedang.net <ishu.rana@vedang.net>
2021-04-01 00:16:38 +0530

    case status history page added

---
+++ LeaseDetails/wwwroot/js/casestatushistory.js
@@ -0,0 +1,42 @@
+﻿
+$(document).ready(function () {
+
+    var id = parseInt($('#RequestForProceedingId').val());
+    GetDetails(id);
+    GetNoticeDetails(id);
+    GetEvidenceDetails(id);
+    GetHearingDetails(id);
+});
+
+
+function GetDetails(id) {
+    HttpGet(`/CaseStatusHistory/RequestForProceedingEvictionView/?id=${id}`, 'html', function (response) {
+        $('#RequestForProceedingEvictionDiv').html("");
+        $('#RequestForProceedingEvictionDiv').html(response);
+    });
+};
+
+
+function GetNoticeDetails(id) {
+    HttpGet(`/CaseStatusHistory/NoticeGenerationView/?id=${id}`, 'html', function (response) {
+        $('#NoticeGenerationDiv').html("");
+        $('#NoticeGenerationDiv').html(response);
+    });
+};
+
+
+
+function GetEvidenceDetails(id) {
+    HttpGet(`/CaseStatusHistory/AllotteeEvidenceView/?id=${id}`, 'html', function (response) {
+        $('#AllotteeEvidenceDiv').html("");
+        $('#AllotteeEvidenceDiv').html(response);
+    });
+};
+
+
+function GetHearingDetails(id) {
+    HttpGet(`/CaseStatusHistory/HearingDetailsView/?id=${id}`, 'html', function (response) {
+        $('#HearingDetailsDiv').html("");
+        $('#HearingDetailsDiv').html(response);
+    });
+};
