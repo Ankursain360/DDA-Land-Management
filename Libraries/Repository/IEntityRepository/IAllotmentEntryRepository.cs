f139ead69366533c5b3313e898c4397ae0a56126
shalini rai r <shalini.rai@vedang.net>
2021-03-31 06:20:54 -0700

    allotment entry code mail functionality added

--- Libraries/Repository/IEntityRepository/IAllotmentEntryRepository.cs
+++ Libraries/Repository/IEntityRepository/IAllotmentEntryRepository.cs
@@ -24,7 +24,7 @@ namespace Libraries.Repository.IEntityRepository
         Task<Premiumrate> FetchSinglerateResult(int? leasePurposeId, int? leaseSubPurposeId, string allotmentDate);
         Task<Groundrent> FetchSinglegroundrentResult(int? leasePurposeId, int? leaseSubPurposeId, string allotmentDate);
         Task<Licencefees> FetchSinglefeeResult(int? leasePurposeId, int? leaseSubPurposeId, string allotmentDate);
-
+        Task<Leaseapplication> FetchLeaseApplicationmailDetails(int id);
 
 
 
