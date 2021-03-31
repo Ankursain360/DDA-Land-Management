43b03ede2596298410c151b5a22ab8f0bc39ec95
Parul <Parul@vedang.net>
2021-03-31 20:06:55 +0530

    Update remaing changes

---
+++ LeaseDetails/wwwroot/js/hearingdetailsletter.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    var todayN = yyyy + '-' + mm + '-' + dd;
+    $("#txttodayDate").val(todayN);
+    var HearingDate = sessionStorage.getItem("NewDate");
+    var HearingTime = sessionStorage.getItem("RefN");
+
+    $("#txtNewDate").val(HearingDate);
+    $("#txtRefN").val(HearingTime);
+   
+});
+$(function () {
+    $("#btnPrint").click(function () {
+        window.print();
+    });
+});
