e42faf03884f44d0a9c42e513a0be3cca9907cc2
Renu Parashar <renu@vedang.net>
2021-04-01 19:12:32 +0530

    Mortgage record added code completed

--- LeaseForPublic/Controllers/MortgageController.cs
+++ LeaseForPublic/Controllers/MortgageController.cs
@@ -45,80 +45,144 @@ namespace LeaseForPublic.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public async Task<PartialViewResult> List([FromBody] CancellationEntrySearchDto model)
-        //{
-        //    var result = await _cancellationEntryService.GetPagedCancellationEntry(model);
-        //    return PartialView("_List", result);
-        //}
+        [HttpPost]
+        public async Task<PartialViewResult> List([FromBody] MortgageSearchDto model)
+        {
+            var result = await _mortgageService.GetPagedMortgageDetails(model);
+            return PartialView("_List", result);
+        }
 
-        ////   [AuthorizeContext(ViewAction.Add)]
-        //public async Task<IActionResult> Create()
-        //{
-        //    Cancellationentry cancellationentry = new Cancellationentry();
-        //    cancellationentry.IsActive = 1;
-        //    cancellationentry.HonbleList = await _cancellationEntryService.GetAllHonble();
-        //    cancellationentry.AllotmententryList = await _cancellationEntryService.GetAllAllotment();
-        //    return View(cancellationentry);
-        //}
+        //   [AuthorizeContext(ViewAction.Add)]
+        public async Task<IActionResult> Create()
+        {
+            Mortgage mortgage = new Mortgage();
+            mortgage.IsActive = 1;
+            mortgage.Documentchecklist = await _mortgageService.GetDocumentChecklistDetails(Convert.ToInt32(_configuration.GetSection("ServiceTypeIdMortgage").Value));
+            return View(mortgage);
+        }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create(Cancellationentry cancellationentry)
-        //{
-        //    try
-        //    {
-        //        cancellationentry.AllotmententryList = await _cancellationEntryService.GetAllAllotment();
-        //        cancellationentry.HonbleList = await _cancellationEntryService.GetAllHonble();
-        //        if (ModelState.IsValid)
-        //        {
-        //            FileHelper fileHelper = new FileHelper();
-        //            if (cancellationentry.DemandLetterPhoto != null)
-        //            {
-        //                cancellationentry.DemandLetter = fileHelper.SaveFile1(targetPathDemandLetter, cancellationentry.DemandLetterPhoto);
-        //            }
-        //            if (cancellationentry.NocPhoto != null)
-        //            {
-        //                cancellationentry.Noc = fileHelper.SaveFile1(targetPathNOC, cancellationentry.NocPhoto);
-        //            }
-        //            if (cancellationentry.CancellationPhoto != null)
-        //            {
-        //                cancellationentry.CancellationOrder = fileHelper.SaveFile1(targetPathCanellationOrder, cancellationentry.CancellationPhoto);
-        //            }
-        //            cancellationentry.CreatedBy = SiteContext.UserId;
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Mortgage mortgage)
+        {
+            try
+            {
+                mortgage.ServiceTypeId = 2;
+                mortgage.Documentchecklist = await _mortgageService.GetDocumentChecklistDetails(Convert.ToInt32(_configuration.GetSection("ServiceTypeIdMortgage").Value));
+                ////Is Mandatory check
+                //if (mortgage.DocumentName != null && mortgage.Mandatory != null)
+                //{
+                //    if (mortgage.DocumentName.Count > 0 && mortgage.Mandatory.Count > 0)
+                //    {
+                //        for (int i = 0; i < mortgage.DocumentName.Count; i++)
+                //        {
+                //            if (mortgage.IsMandatory[i] == 1)
+                //            {
+                //                if (mortgage.FileUploaded == null || mortgage.FileUploaded.Count <= i)
+                //                {
+                //                    ViewBag.Message = Alert.Show("Please Upload Mandatory Documents", "", AlertType.Warning);
+                //                    return View(mortgage);
 
-        //            cancellationentry.CreatedBy = SiteContext.UserId;
-        //            var result = await _cancellationEntryService.Create(cancellationentry);
-        //            if (result == true)
-        //            {
-        //                ViewBag.Message = Alert.Show(Messages.AddRecordSuccess, "", AlertType.Success);
-        //                var list = await _cancellationEntryService.GetAllRequestForProceeding();
-        //                return View("Index", list);
-        //            }
-        //            else
-        //            {
-        //                ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
-        //                return View(cancellationentry);
-        //            }
-        //        }
-        //        else
-        //        {
-        //            return View(cancellationentry);
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
-        //        return View(cancellationentry);
-        //    }
-        //}
+                //                }
+                //            }
+                //        }
+                //    }
+                //}
+                if (ModelState.IsValid)
+                {
+                    FileHelper fileHelper = new FileHelper();
+                    mortgage.CreatedBy = 1;
+                    mortgage.ApprovedStatus = 0;
+                    mortgage.PendingAt = "1";
+                    mortgage.IsActive = 1;
+                    mortgage.Id = 0;
+                    var result = await _mortgageService.Create(mortgage);
+
+                    if (result)
+                    {
+                        List<Allotteeservicesdocument> allotteeservicesdocuments = new List<Allotteeservicesdocument>();
+                        for (int i = 0; i < mortgage.DocumentChecklistId.Count; i++)
+                        {
+                            string filename = null;
+                            if (mortgage.FileUploaded != null && mortgage.FileUploaded.Count > 0)
+                                filename = mortgage.FileUploaded != null ?
+                                                                   mortgage.FileUploaded.Count <= i ? string.Empty :
+                                                                   fileHelper.SaveFile1(targetPathMortgageDocuments, mortgage.FileUploaded[i]) :
+                                                                   mortgage.FileUploaded[i] != null || mortgage.FileUploadedPath[i] != "" ?
+                                                                   mortgage.FileUploadedPath[i] : string.Empty;
+                            allotteeservicesdocuments.Add(new Allotteeservicesdocument
+                            {
+                                DocumentChecklistId = mortgage.DocumentChecklistId.Count <= i ? 0 : mortgage.DocumentChecklistId[i],
+                                ServiceId = mortgage.Id,
+                                ServiceTypeId = mortgage.ServiceTypeId,
+                                DocumentFileName = filename,
+                                CreatedBy = SiteContext.UserId
+
+                            });
+                        }
+                        if (allotteeservicesdocuments.Count > 0)
+                            result = await _mortgageService.SaveAllotteeServiceDocuments(allotteeservicesdocuments);
+
+                    }
+                    if (result == true)
+                    {
+                        //#region Approval Proccess At 1st level start Added by Renu 16 March 2021
+                        //var DataFlow = await dataAsync();
+                        //for (int i = 0; i < DataFlow.Count; i++)
+                        //{
+                        //    if (!DataFlow[i].parameterSkip)
+                        //    {
+                        //        mortgage.ApprovedStatus = 0;
+                        //        mortgage.PendingAt = Convert.ToInt32(DataFlow[i].parameterName);
+                        //        result = await _leaseApplicationFormService.UpdateBeforeApproval(mortgage.Id, mortgage);  //Update Table details 
+                        //        if (result)
+                        //        {
+                        //            Approvalproccess approvalproccess = new Approvalproccess();
+                        //            approvalproccess.ModuleId = Convert.ToInt32(_configuration.GetSection("approvalModuleId").Value);
+                        //            approvalproccess.ProccessID = Convert.ToInt32(_configuration.GetSection("workflowPreccessIdLeaseApplicationForm").Value);
+                        //            approvalproccess.ServiceId = mortgage.Id;
+                        //            approvalproccess.SendFrom = SiteContext.UserId;
+                        //            approvalproccess.SendTo = Convert.ToInt32(DataFlow[i].parameterName);
+                        //            approvalproccess.PendingStatus = 1;   //1
+                        //            approvalproccess.Status = null;   //1
+                        //            approvalproccess.Remarks = "Record Added and Send for Approval";///May be Uncomment
+                        //            result = await _approvalproccessService.Create(approvalproccess, SiteContext.UserId); //Create a row in approvalproccess Table
+                        //        }
+
+                        //        break;
+                        //    }
+                        //}
+
+                        //#endregion 
+
+                        ViewBag.Message = Alert.Show(Messages.AddAndApprovalRecordSuccess, "", AlertType.Success);
+                        return View("Index");
+                    }
+                    else
+                    {
+                        ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                        return View(mortgage);
+
+                    }
+                }
+                else
+                {
+                    return View(mortgage);
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                return View(mortgage);
+            }
+        }
 
         //[AuthorizeContext(ViewAction.Edit)]
         //public async Task<IActionResult> Edit(int id)
         //{
-        //    var Data = await _cancellationEntryService.FetchSingleResult(id);
-        //    Data.HonbleList = await _cancellationEntryService.GetAllHonble();
-        //    Data.AllotmententryList = await _cancellationEntryService.GetAllAllotment();
+        //    var Data = await _mortgageService.FetchSingleResult(id);
+        //    Data.HonbleList = await _mortgageService.GetAllHonble();
+        //    Data.AllotmententryList = await _mortgageService.GetAllAllotment();
         //    if (Data == null)
         //    {
         //        return NotFound();
@@ -130,8 +194,8 @@ namespace LeaseForPublic.Controllers
         //[ValidateAntiForgeryToken]
         //public async Task<IActionResult> Edit(int id, Cancellationentry cancellationentry)
         //{
-        //    cancellationentry.AllotmententryList = await _cancellationEntryService.GetAllAllotment();
-        //    cancellationentry.HonbleList = await _cancellationEntryService.GetAllHonble();
+        //    cancellationentry.AllotmententryList = await _mortgageService.GetAllAllotment();
+        //    cancellationentry.HonbleList = await _mortgageService.GetAllHonble();
 
         //    FileHelper fileHelper = new FileHelper();
         //    if (cancellationentry.DemandLetterPhoto != null)
@@ -149,11 +213,11 @@ namespace LeaseForPublic.Controllers
         //    try
         //    {
         //        cancellationentry.ModifiedBy = SiteContext.UserId;
-        //        var result = await _cancellationEntryService.Update(id, cancellationentry);
+        //        var result = await _mortgageService.Update(id, cancellationentry);
         //        if (result == true)
         //        {
         //            ViewBag.Message = Alert.Show(Messages.UpdateRecordSuccess, "", AlertType.Success);
-        //            var list = await _cancellationEntryService.GetAllRequestForProceeding();
+        //            var list = await _mortgageService.GetAllRequestForProceeding();
         //            return View("Index", list);
         //        }
         //        else
@@ -173,9 +237,9 @@ namespace LeaseForPublic.Controllers
         //[AuthorizeContext(ViewAction.View)]
         //public async Task<IActionResult> View(int id)
         //{
-        //    var Data = await _cancellationEntryService.FetchSingleResult(id);
-        //    Data.HonbleList = await _cancellationEntryService.GetAllHonble();
-        //    Data.AllotmententryList = await _cancellationEntryService.GetAllAllotment();
+        //    var Data = await _mortgageService.FetchSingleResult(id);
+        //    Data.HonbleList = await _mortgageService.GetAllHonble();
+        //    Data.AllotmententryList = await _mortgageService.GetAllAllotment();
         //    if (Data == null)
         //    {
         //        return NotFound();
@@ -188,7 +252,7 @@ namespace LeaseForPublic.Controllers
         //{
         //    try
         //    {
-        //        var result = await _cancellationEntryService.Delete(id);
+        //        var result = await _mortgageService.Delete(id);
         //        if (result == true)
         //        {
         //            ViewBag.Message = Alert.Show(Messages.DeleteSuccess, "", AlertType.Success);
@@ -202,7 +266,7 @@ namespace LeaseForPublic.Controllers
         //    {
         //        ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
         //    }
-        //    var list = await _cancellationEntryService.GetAllRequestForProceeding();
+        //    var list = await _mortgageService.GetAllRequestForProceeding();
         //    return View("Index", list);
         //}
 
@@ -211,7 +275,7 @@ namespace LeaseForPublic.Controllers
         //    try
         //    {
         //        FileHelper file = new FileHelper();
-        //        var Data = await _cancellationEntryService.FetchSingleResult(Id);
+        //        var Data = await _mortgageService.FetchSingleResult(Id);
         //        string targetPhotoPathLayout = targetPathDemandLetter + Data.DemandLetter;
         //        byte[] FileBytes = System.IO.File.ReadAllBytes(targetPhotoPathLayout);
         //        return File(FileBytes, file.GetContentType(targetPhotoPathLayout));
@@ -220,7 +284,7 @@ namespace LeaseForPublic.Controllers
         //    {
 
         //        FileHelper file = new FileHelper();
-        //        var Data = await _cancellationEntryService.FetchSingleResult(Id);
+        //        var Data = await _mortgageService.FetchSingleResult(Id);
         //        string targetPhotoPathLayout = targetPathDemandLetter + Data.DemandLetter;
         //        byte[] FileBytes = System.IO.File.ReadAllBytes(targetPhotoPathLayout);
         //        return File(FileBytes, file.GetContentType(targetPhotoPathLayout));
@@ -233,7 +297,7 @@ namespace LeaseForPublic.Controllers
         //    try
         //    {
         //        FileHelper file = new FileHelper();
-        //        var Data = await _cancellationEntryService.FetchSingleResult(Id);
+        //        var Data = await _mortgageService.FetchSingleResult(Id);
         //        string targetPhotoPathLayout = targetPathNOC + Data.Noc;
         //        byte[] FileBytes = System.IO.File.ReadAllBytes(targetPhotoPathLayout);
         //        return File(FileBytes, file.GetContentType(targetPhotoPathLayout));
@@ -242,7 +306,7 @@ namespace LeaseForPublic.Controllers
         //    {
 
         //        FileHelper file = new FileHelper();
-        //        var Data = await _cancellationEntryService.FetchSingleResult(Id);
+        //        var Data = await _mortgageService.FetchSingleResult(Id);
         //        string targetPhotoPathLayout = targetPathNOC + Data.Noc;
         //        byte[] FileBytes = System.IO.File.ReadAllBytes(targetPhotoPathLayout);
         //        return File(FileBytes, file.GetContentType(targetPhotoPathLayout));
@@ -254,7 +318,7 @@ namespace LeaseForPublic.Controllers
         //    try
         //    {
         //        FileHelper file = new FileHelper();
-        //        var Data = await _cancellationEntryService.FetchSingleResult(Id);
+        //        var Data = await _mortgageService.FetchSingleResult(Id);
         //        string targetPhotoPathLayout = targetPathCanellationOrder + Data.CancellationOrder;
         //        byte[] FileBytes = System.IO.File.ReadAllBytes(targetPhotoPathLayout);
         //        return File(FileBytes, file.GetContentType(targetPhotoPathLayout));
@@ -263,19 +327,18 @@ namespace LeaseForPublic.Controllers
         //    {
 
         //        FileHelper file = new FileHelper();
-        //        var Data = await _cancellationEntryService.FetchSingleResult(Id);
+        //        var Data = await _mortgageService.FetchSingleResult(Id);
         //        string targetPhotoPathLayout = targetPathCanellationOrder + Data.CancellationOrder;
         //        byte[] FileBytes = System.IO.File.ReadAllBytes(targetPhotoPathLayout);
         //        return File(FileBytes, file.GetContentType(targetPhotoPathLayout));
 
         //    }
         //}
-        //[HttpGet]
-        //public async Task<JsonResult> GetOtherData(int? AllottmentId)
-        //{
-        //    AllottmentId = AllottmentId ?? 0;
-        //    return Json(await _cancellationEntryService.FetchAllottmentDetails(Convert.ToInt32(AllottmentId)));
-        //}
+        [HttpGet]
+        public async Task<JsonResult> GetOtherData()
+        {            
+            return Json(await _mortgageService.GetAllotteeDetails(SiteContext.UserId));
+        }
 
     }
 }
--- Libraries/Repository/EntityRepository/MortgageRepository.cs
+++ Libraries/Repository/EntityRepository/MortgageRepository.cs
@@ -22,5 +22,107 @@ namespace Libraries.Repository.EntityRepository
         {
 
         }
+
+        public async Task<Possesionplan> GetAllotteeDetails(int userId)
+        {
+            return await _dbContext.Possesionplan
+                                    .Include(x => x.Allotment)
+                                    .Include(x => x.Allotment.Application)
+                                    .Include(x => x.Allotment.LeasePurposesType)
+                                    .Where(x => x.Allotment.Application.UserId == userId)
+                                    .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Documentchecklist>> GetDocumentChecklistDetails(int servicetypeid)
+        {
+            return await _dbContext.Documentchecklist
+                                        .Include(x => x.ServiceType)
+                                        .Where(x => x.ServiceTypeId == servicetypeid)
+                                        .OrderByDescending(x => x.IsMandatory)
+                                        .ToListAsync();
+        }
+
+        public async Task<PagedResult<Mortgage>> GetPagedMortgageDetails(MortgageSearchDto model)
+        {
+            var data = await _dbContext.Mortgage
+                                         .Include(x => x.Allottment)
+                                         .Include(x => x.Allottment.Application)
+                                         .Where(x => (x.Allottment.Application.RefNo != null ? x.Allottment.Application.RefNo.Contains(model.refno == "" ? x.Allottment.Application.RefNo : model.refno) : true)
+                                         && (x.Allottment.Application.Name != null ? x.Allottment.Application.Name.Contains(model.name == "" ? x.Allottment.Application.Name : model.name) : true)
+                                        )
+                                         .GetPaged<Mortgage>(model.PageNumber, model.PageSize);
+            int SortOrder = (int)model.SortOrder;
+            if (SortOrder == 1)
+            {
+                data = null;
+                if (model.SortBy.ToUpper() == "ISACTIVE")
+                {
+                    data = await _dbContext.Mortgage
+                                         .Include(x => x.Allottment)
+                                         .Include(x => x.Allottment.Application)
+                                         .Where(x => (x.Allottment.Application.RefNo != null ? x.Allottment.Application.RefNo.Contains(model.refno == "" ? x.Allottment.Application.RefNo : model.refno) : true)
+                                         && (x.Allottment.Application.Name != null ? x.Allottment.Application.Name.Contains(model.name == "" ? x.Allottment.Application.Name : model.name) : true)
+                                        )
+                                        .OrderByDescending(s => s.IsActive)
+                                         .GetPaged<Mortgage>(model.PageNumber, model.PageSize);
+                }
+                else
+                {
+                    data = null;
+                    data = await _dbContext.Mortgage
+                                         .Include(x => x.Allottment)
+                                         .Include(x => x.Allottment.Application)
+                                         .Where(x => (x.Allottment.Application.RefNo != null ? x.Allottment.Application.RefNo.Contains(model.refno == "" ? x.Allottment.Application.RefNo : model.refno) : true)
+                                         && (x.Allottment.Application.Name != null ? x.Allottment.Application.Name.Contains(model.name == "" ? x.Allottment.Application.Name : model.name) : true)
+                                        )
+                                       .OrderBy(s =>
+                                       (model.SortBy.ToUpper() == "REFNO" ? (s.Allottment == null ? null : s.Allottment.Application == null ? null : s.Allottment.Application.RefNo)
+                                       : model.SortBy.ToUpper() == "SOCIETYNAME" ? (s.Allottment == null ? null : s.Allottment.Application == null ? null : s.Allottment.Application.Name)
+                                       : (s.Allottment == null ? null : s.Allottment.Application == null ? null : s.Allottment.Application.RefNo))
+                                       )
+                                        .GetPaged<Mortgage>(model.PageNumber, model.PageSize);
+                }
+
+            }
+            else if (SortOrder == 2)
+            {
+                if (model.SortBy.ToUpper() == "ISACTIVE")
+                {
+                    data = await _dbContext.Mortgage
+                                         .Include(x => x.Allottment)
+                                         .Include(x => x.Allottment.Application)
+                                         .Where(x => (x.Allottment.Application.RefNo != null ? x.Allottment.Application.RefNo.Contains(model.refno == "" ? x.Allottment.Application.RefNo : model.refno) : true)
+                                         && (x.Allottment.Application.Name != null ? x.Allottment.Application.Name.Contains(model.name == "" ? x.Allottment.Application.Name : model.name) : true)
+                                        )
+                                        .OrderBy(s => s.IsActive)
+                                         .GetPaged<Mortgage>(model.PageNumber, model.PageSize);
+                }
+                else
+                {
+                    data = null;
+                    data = await _dbContext.Mortgage
+                                         .Include(x => x.Allottment)
+                                         .Include(x => x.Allottment.Application)
+                                         .Where(x => (x.Allottment.Application.RefNo != null ? x.Allottment.Application.RefNo.Contains(model.refno == "" ? x.Allottment.Application.RefNo : model.refno) : true)
+                                         && (x.Allottment.Application.Name != null ? x.Allottment.Application.Name.Contains(model.name == "" ? x.Allottment.Application.Name : model.name) : true)
+                                        )
+                                       .OrderByDescending(s =>
+                                       (model.SortBy.ToUpper() == "REFNO" ? (s.Allottment == null ? null : s.Allottment.Application == null ? null : s.Allottment.Application.RefNo)
+                                       : model.SortBy.ToUpper() == "SOCIETYNAME" ? (s.Allottment == null ? null : s.Allottment.Application == null ? null : s.Allottment.Application.Name)
+                                       : (s.Allottment == null ? null : s.Allottment.Application == null ? null : s.Allottment.Application.RefNo))
+                                       )
+                                        .GetPaged<Mortgage>(model.PageNumber, model.PageSize);
+                }
+
+            }
+            return data;
+        }
+
+        public async Task<bool> SaveAllotteeServiceDocuments(List<Allotteeservicesdocument> allotteeservicesdocuments)
+        {
+            await _dbContext.Allotteeservicesdocument.AddRangeAsync(allotteeservicesdocuments);
+            var Result = await _dbContext.SaveChangesAsync();
+            return Result > 0 ? true : false;
+        }
     }
 }
