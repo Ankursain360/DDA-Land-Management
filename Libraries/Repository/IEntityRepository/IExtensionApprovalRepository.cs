bfb6a9ceee4c12dcc811063c95718d5389af8c00
ishu.rana@vedang.net <ishu.rana@vedang.net>
2021-04-07 22:30:39 +0530

    index and create coding done for extension approval

---
+++ Libraries/Repository/IEntityRepository/IExtensionApprovalRepository.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dto.Search;
+using Libraries.Model.Entity;
+using Libraries.Repository.Common;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Libraries.Repository.IEntityRepository
+{
+    public interface IExtensionApprovalRepository : IGenericRepository<Extension>
+    {
+        Task<PagedResult<Extension>> GetPagedExtensionDetails(ExtensionApprovalSearchDto model, int userId);
+        Task<Extension> FetchSingleResult(int id);
+    }
+}
---
+++ Libraries/Service/ApplicationService/ExtensionApprovalService.cs
@@ -0,0 +1,44 @@
+﻿
+
+using Libraries.Model.Entity;
+using Libraries.Repository.Common;
+using Libraries.Service.Common;
+using Libraries.Service.IApplicationService;
+using Libraries.Repository.IEntityRepository;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Dto.Search;
+using Dto.Master;
+using AutoMapper;
+
+namespace Libraries.Service.ApplicationService
+{
+
+    public class ExtensionApprovalService : EntityService<Extension>, IExtensionApprovalService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IExtensionApprovalRepository _extensionApprovalRepository;
+        private readonly IMapper _mapper;
+        public ExtensionApprovalService(IUnitOfWork unitOfWork,
+            IExtensionApprovalRepository extensionApprovalRepository,
+            IMapper mapper)
+        : base(unitOfWork, extensionApprovalRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _extensionApprovalRepository = extensionApprovalRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Extension> FetchSingleResult(int id)
+        {
+            return await _extensionApprovalRepository.FetchSingleResult(id);
+        }
+
+        public async Task<PagedResult<Extension>> GetPagedExtensionDetails(ExtensionApprovalSearchDto model, int userId)
+        {
+            return await _extensionApprovalRepository.GetPagedExtensionDetails(model, userId);
+        }
+    }
+}
