3d3e814fd42e7fead59d5361be425a7d68c1dade
Pankaj Bhandari <Pankaj.bhandari@vedng.net>
2021-05-05 19:59:52 +0530

    Download functionality added in Allotment Entry

---
+++ Libraries/Dto/Master/AllotmentEntryListDto.cs
@@ -0,0 +1,24 @@
+﻿using Dto.Common;
+
+
+namespace Dto.Master
+{
+    public class AllotmentEntryListDto
+    {
+        public int Id { get; set; }
+        public string AllicationName { get; set; }
+        public string TotalArea { get; set; }
+
+        public string PlayGroundArea { get; set; }
+        public string AllotmentDate { get; set; }
+
+        public string PhaseNo { get; set; }
+
+        public string SectorNo { get; set; }
+
+        public string PocketNo { get; set; }
+
+        public string PlotNo { get; set; }
+        public string Status { get; set; }
+    }
+}
