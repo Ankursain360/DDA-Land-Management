89a8cb02fd7f3be8e8e1796cd76840f9ff16621b
Pankaj Bhandari <Pankaj.bhandari@vedng.net>
2021-05-10 19:49:01 +0530

    Public Link Add in Login Page

---
+++ Libraries/Dto/Master/WorkFlowTemplateListDto.cs
@@ -0,0 +1,15 @@
+﻿using Dto.Common;
+
+namespace Dto.Master
+{
+     public  class WorkFlowTemplateListDto
+    {
+        public int Id { get; set; }
+        public string Version { get; set; }
+        public string Module { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SLATimeLine { get; set; }
+        public string IsActive { get; set; }
+    }
+}
