6f868bb9b4c70dcb1acc4ff7fd01a7cdf084f34a
ishu.rana@vedang.net <ishu.rana@vedang.net>
2021-05-25 18:38:05 +0530

    damage paye public interface module payment page coding done

---
+++ Libraries/Service/ApplicationService/DPPublicPaymentService.cs
@@ -0,0 +1,39 @@
+﻿
+using Dto.Search;
+using Libraries.Model.Entity;
+using Libraries.Repository.Common;
+using Libraries.Repository.IEntityRepository;
+using Libraries.Service.Common;
+using Libraries.Service.IApplicationService;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.ApplicationService
+{
+    public class DPPublicPaymentService : EntityService<Demandletters>, IDPPublicPaymentService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IDPPublicPaymentRepository _dPPublicPaymentRepository;
+     
+        public DPPublicPaymentService(IUnitOfWork unitOfWork, IDPPublicPaymentRepository dPPublicPaymentRepository)
+        : base(unitOfWork, dPPublicPaymentRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _dPPublicPaymentRepository = dPPublicPaymentRepository;
+            
+        }
+
+        public async Task<Damagepayeeregister> FetchDamagePayeeRegisterDetails(int userId)
+        {
+            return await _dPPublicPaymentRepository.FetchDamagePayeeRegisterDetails(userId);
+        }
+        public async Task<List<Demandletters>> GetDemandDetails(string FileNo)
+        {
+            return await _dPPublicPaymentRepository.GetDemandDetails(FileNo);
+        }
+    }
+}
---
+++ Libraries/Service/IApplicationService/IDPPublicPaymentService.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+using Dto.Search;
+using Libraries.Model.Entity;
+using Libraries.Repository.Common;
+using Libraries.Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraries.Service.IApplicationService
+{
+
+    public interface IDPPublicPaymentService : IEntityService<Demandletters>
+    {
+        Task<Damagepayeeregister> FetchDamagePayeeRegisterDetails(int userId);
+        // Task<PagedResult<Damagepayeeregister>> GetPagedDamagepayeeregister(DamagepayeeregistertempSearchDto model);
+        Task<List<Demandletters>> GetDemandDetails(string FileNo);
+
+    }
+}
