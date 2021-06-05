903ad50732851dee3d1e4562b1920fcf7319b2de
ishu.rana@vedang.net <ishu.rana@vedang.net>
2021-06-05 22:40:17 +0530

    compactor details page added in file data loading module

---
+++ FileDataLoading/Controllers/AllCompactorDtlsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Dto.Master;
+using Microsoft.AspNetCore.Mvc;
+//using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace FileDataLoading.Controllers
+{
+    public class AllCompactorDtlsController : Controller
+    {
+        public IConfiguration _configuration;
+        public AllCompactorDtlsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+           
+        }
+        public async Task<IActionResult> Index()
+        {
+              
+        List<CompactorApiDto> CompactorList = new List<CompactorApiDto>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(_configuration.GetSection("compactorallApi").Value))
+               // using (var response = await httpClient.GetAsync("http://119.226.139.196/compactorfilesearch/Damage.asmx/GetCompactorDtls_All"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                   // var data1 = JsonSerializer.Deserialize<List<CompactorApiDto>>(apiResponse);
+                    var data2 = JsonSerializer.Deserialize<ApiResponseCompactor>(apiResponse);
+
+                    return View(data2);
+                }
+            }
+        }
+    }
+}
---
+++ Libraries/Dto/Master/CompactorApiDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dto.Master
+{
+    public class CompactorApiDto
+    {
+        public string MAIN_FILE_NO { get; set; }
+        public string PART_FILE_NO { get; set; }
+        public string SUBJECT { get; set; }
+        public string NAME { get; set; }
+        public int RM_NO { get; set; }
+        public string CMPCTR_NO { get; set; }
+        public int RW_NO { get; set; }
+        public string CLMN_NO { get; set; }
+        public string ALMRH_NO { get; set; }
+        public string FLT_CTGRY_ID { get; set; }
+        public string LCLTY_HDR { get; set; }
+        public string SQNC_NO { get; set; }
+        public int YR { get; set; }
+    }
+
+    public class ApiResponseCompactor
+    {
+        public List<CompactorApiDto> cargo { get; set; }
+    }
+}
