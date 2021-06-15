d6c0cf17536193a3636ed05cc50b1548ce46c462
ishu.rana@vedang.net <ishu.rana@vedang.net>
2021-06-15 22:17:35 +0530

    Police Assistance letter changes done

--- Libraries/Model/Entity/Demolitionpoliceassistenceletter.cs
+++ Libraries/Model/Entity/Demolitionpoliceassistenceletter.cs
@@ -11,27 +11,41 @@ namespace Libraries.Model.Entity
 {
     public class Demolitionpoliceassistenceletter : AuditableEntity<int>
     {
+      
         public int FixingDemolitionId { get; set; }
-
+        public string OfficeName { get; set; }
+        public string OfficeDepartment { get; set; }
+        public string OfficeZone { get; set; }
+        public string OfficeAddress { get; set; }
+        public string FileNo { get; set; }
+        public DateTime? LetterDate { get; set; }
+        public string DyCommOffcAddress { get; set; }
+        public string KhasraNo { get; set; }
+        public string VillageName { get; set; }
+        public string KhasraAddress { get; set; }
         [Required(ErrorMessage = "Meeting Date is mandatory field")]
         public DateTime? MeetingDate { get; set; }
 
         [Required(ErrorMessage = "Meeting Time is mandatory field")]
         public string MeetingTime { get; set; }
+        public string GeneralConditions { get; set; }
+        public string ChiefEngineerAddress { get; set; }
+        public string Shoaddress { get; set; }
 
         [Required(ErrorMessage = "Police Station Name is mandatory field")]
         public string PoliceStationName { get; set; }
+        public DateTime? OperationDate { get; set; }
+        public string OperationDay { get; set; }
+        public string RevenueOfficerZone { get; set; }
+        public string RevenueOfficerWing { get; set; }
+        public string RevenueOfficerBranch { get; set; }
+      
+        public byte? IsActive { get; set; }
 
-        [Required(ErrorMessage = "Address/KhasraNo is mandatory field")]
-        public string AddressKhasraNo { get; set; }
-
-        [Required(ErrorMessage = "Religious is mandatory field")]
-        public string IsReligiousStructure { get; set; }
-        public string FilePath { get; set; }
         public Fixingdemolition FixingDemolition { get; set; }
 
-        [NotMapped]
-        public IFormFile Document { get; set; }
+        //[NotMapped]
+        //public IFormFile Document { get; set; }
 
         [NotMapped]
         public int GenerateUpload { get; set; }
