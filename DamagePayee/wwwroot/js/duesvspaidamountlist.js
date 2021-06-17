6b94ef4d723b0be79134311625698172cd1c9680
nikita <nikita@vedang.net>
2021-06-17 12:22:00 +0530

    Changes in dues vs paid amount js file

--- DamagePayee/wwwroot/js/duesvspaidamountlist.js
+++ DamagePayee/wwwroot/js/duesvspaidamountlist.js
@@ -1,20 +1,25 @@
 ﻿$(document).ready(function () {
 
     $("#btnGenerate").click(function () {
-      
-     
 
-        //var fromDate = $('#FromDateMsg').val();
-        //var toDate = $('#ToDateMsg').val();
-        var param = GetSearchParam();
-        alert(JSON.stringify(param));
-            
-        HttpPost(`/DuesVsPaidAmountReport/GetPaidVsDemandList`, 'html', param, function (response) {
-            $('#LoadDuesVsPaidAmount').html("");
-            $('#LoadDuesVsPaidAmount').html(response);
+
+
+        var fromDate = $('#FromDateMsg').val();
+        var toDate = $('#ToDateMsg').val();
+        var Fileno = $('#FileNo').val();
+        if (fromDate == "" || toDate == "" || Fileno == "") {
+            alert("Please Fill All Fields");
+        } else {
+            var param = GetSearchParam();
+          //  alert(JSON.stringify(param));
+
+            HttpPost(`/DuesVsPaidAmountReport/GetPaidVsDemandList`, 'html', param, function (response) {
+                $('#LoadDuesVsPaidAmount').html("");
+                $('#LoadDuesVsPaidAmount').html(response);
             });
-       
-    });
+        }
+        });
+        
 
 });
 
