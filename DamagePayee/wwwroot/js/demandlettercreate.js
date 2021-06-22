4bc60a444ff4ea647afd4ac362dada1f862a8878
nikita <nikita@vedang.net>
2021-06-23 00:31:25 +0530

    changes demand letter

---
+++ DamagePayee/wwwroot/js/demandlettercreate.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+
+    $("#InterestAmount,#Penalty,#DamageCharges,#ReliefAmount").keyup(function () {
+        var InterestAmount = $('#InterestAmount').val();
+        var Penalty = $('#Penalty').val();
+        var DamageCharges = $('#DamageCharges').val();
+        var ReliefAmount = $('#ReliefAmount').val();
+        if (InterestAmount && Penalty && DamageCharges && ReliefAmount) {
+            var total = parseFloat(InterestAmount) + parseFloat(Penalty) + parseFloat(DamageCharges) - parseFloat(ReliefAmount);
+            $("#DepositDue").val(total);
+
+        }
+    });
+});
