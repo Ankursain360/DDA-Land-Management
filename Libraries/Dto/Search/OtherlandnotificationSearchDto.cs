fdc0220b1a61f16231452d3ced84b2c0fe75ead7
nikita <nikita@vedang.net>
2021-06-23 20:43:08 +0530

    Add other land notification in site master

---
+++ Libraries/Dto/Search/OtherlandnotificationSearchDto.cs
@@ -0,0 +1,10 @@
+﻿using Dto.Common;
+namespace Dto.Search
+{
+    public class OtherlandnotificationSearchDto : BaseSearchDto
+    {
+        public string landtype { get; set; }
+        public string notificationnumber { get; set; }
+        
+    }
+}
