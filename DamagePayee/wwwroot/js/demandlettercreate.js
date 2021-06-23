4421bf744d918db7659a7c9bf7ae0e9fcb979f46
nikita <nikita@vedang.net>
2021-06-23 11:42:09 +0530

    changes in demand letter

--- DamagePayee/wwwroot/js/demandlettercreate.js
+++ DamagePayee/wwwroot/js/demandlettercreate.js
@@ -2,10 +2,17 @@
 
     $("#InterestAmount,#Penalty,#DamageCharges,#ReliefAmount").keyup(function () {
         var InterestAmount = $('#InterestAmount').val();
+        if (InterestAmount) {
+
+        } else {
+            InterestAmount = 0;
+        }
         var Penalty = $('#Penalty').val();
+
+
         var DamageCharges = $('#DamageCharges').val();
         var ReliefAmount = $('#ReliefAmount').val();
-        if (InterestAmount && Penalty && DamageCharges && ReliefAmount) {
+        if (Penalty && DamageCharges && ReliefAmount) {
             var total = parseFloat(InterestAmount) + parseFloat(Penalty) + parseFloat(DamageCharges) - parseFloat(ReliefAmount);
             $("#DepositDue").val(total);
 
