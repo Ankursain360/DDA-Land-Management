5ec583deeb781890abe4dee042d00349278e0829
nikita <nikita@vedang.net>
2021-06-23 16:00:17 +0530

    Add entity other land notification

---
+++ Libraries/Model/Entity/Otherlandnotification.cs
@@ -0,0 +1,18 @@
+﻿using Libraries.Model.Common;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Libraries.Model.Entity
+{
+    public  class Otherlandnotification : AuditableEntity<int>
+    {
+      
+        public string LandType { get; set; }
+        public string NotificationNumber { get; set; }
+      
+        public byte? IsActive { get; set; }
+    }
+}
---
+++ Libraries/Model/EntityConfiguration/OtherlandnotificationConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Libraries.Model.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Libraries.Model.EntityConfiguration
+{
+  public  class OtherlandnotificationConfiguration : IEntityTypeConfiguration<Otherlandnotification>
+    {
+        public void Configure(EntityTypeBuilder<Otherlandnotification> builder)
+        {
+
+
+            builder.ToTable("otherlandnotification", "lms");
+
+            builder.Property(e => e.Id)
+                .HasColumnName("id")
+                .HasColumnType("int(11)");
+
+            builder.Property(e => e.CreatedBy).HasColumnType("int(11)");
+
+            builder.Property(e => e.CreatedDate).HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            builder.Property(e => e.IsActive).HasColumnType("tinyint(4)");
+
+            builder.Property(e => e.LandType)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+
+            builder.Property(e => e.ModifiedBy).HasColumnType("int(11)");
+
+            builder.Property(e => e.ModifiedDate).HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            builder.Property(e => e.NotificationNumber)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+
+        }
+        }
+    }
