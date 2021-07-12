aaabea342f38fbbf84bb131bedbb74af1d14243d
ishu.rana@vedang.net <ishu.rana@vedang.net>
2021-07-12 13:43:49 +0530

    kyc form design done

---
+++ LeaseForPublic/wwwroot/js/kycform.js
@@ -0,0 +1,47 @@
+﻿
+    $(function () {
+        var value = $('#ddproperty option:selected').val();
+        if (value == "Lease") {
+        $('#divLeaseproperty').show();
+            $('#divLicenseproperty').hide();
+        }
+        else {
+        $('#divLeaseproperty').hide();
+            $('#divLicenseproperty').show();
+        }
+
+        var value2 = $('#ddleasetype option:selected').val();
+        if (value2 == "License") {
+            $('#divTenure').show();
+           
+        }
+        else {
+            $('#divTenure').hide();
+           
+        }
+        
+    });
+    $('#ddproperty').change(function () {
+        var value = $('#ddproperty option:selected').val();
+        if (value == "Lease") {
+        $('#divLeaseproperty').show();
+            $('#divLicenseproperty').hide();
+        }
+        else {
+        $('#divLeaseproperty').hide();
+            $('#divLicenseproperty').show();
+        }
+    });
+
+
+$('#ddleasetype').change(function () {
+    var value2 = $('#ddleasetype option:selected').val();
+    if (value2 == "License") {
+        $('#divTenure').show();
+
+    }
+    else {
+        $('#divTenure').hide();
+
+    }
+});
