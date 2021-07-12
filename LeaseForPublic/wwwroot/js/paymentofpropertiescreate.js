c1897070b9817d6967e4a6e01235b270cac3e327
nikita <nikita@vedang.net>
2021-07-12 15:16:35 +0530

    Add Payment of properties

---
+++ LeaseForPublic/wwwroot/js/paymentofpropertiescreate.js
@@ -0,0 +1,26 @@
+﻿$("#payble1").change(function () {
+    var id = $(this).val();
+
+    if (id == 1) {
+        $("#showyes").show();
+        $("#shoshow").show();
+        $("#payble2").select2();
+        $("#tbl_posts").hide();
+    } else {
+        $("#tbl_posts").hide();
+        $("#shoshow").hide();
+        $("#showyes").hide();
+    }
+});
+
+$("#payble2").change(function () {
+    var id = $(this).val();
+
+    if (id == 1) {
+
+        $("#tbl_posts").hide();
+    } else {
+        $("#tbl_posts").show();
+
+    }
+});
