3db8007d709753f0449a6cbd04c839a00ba1b423
Pankaj Bhandari <Pankaj.bhandari@vedng.net>
2021-07-29 11:58:24 +0530

    Entity Added in Payment Details

---
+++ Libraries/Model/Entity/Kycdemandpaymentdetailstableb.cs
@@ -0,0 +1,23 @@
+﻿using Libraries.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Libraries.Model.Entity
+{
+    public class Kycdemandpaymentdetailstableb : AuditableEntity<int>
+    {
+       
+        public int KycId { get; set; }
+        public int DemandPaymentId { get; set; }
+        public string ChallanNo { get; set; }
+        public string ChallanAmount { get; set; }
+        public DateTime? DepositeDate { get; set; }
+        public byte IsActive { get; set; }
+      
+        public Kycdemandpaymentdetails DemandPayment { get; set; }
+        public Kycform Kyc { get; set; }
+    }
+}
