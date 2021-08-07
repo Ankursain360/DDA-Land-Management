6fa65f4da2e9c698f261297ea4b1024f766039dc
nikita <nikita@vedang.net>
2021-08-07 17:30:01 +0530

    Add validation

--- Libraries/Model/Entity/Kycdemandpaymentdetailstablec.cs
+++ Libraries/Model/Entity/Kycdemandpaymentdetailstablec.cs
@@ -14,11 +14,16 @@ namespace Libraries.Model.Entity
         public int KycId { get; set; }
         public int DemandPaymentId { get; set; }
         public string IsVerified { get; set; }
+        [Required(ErrorMessage = " Payment Type is mandatory")]
         public string PaymentType { get; set; }
         public string Period { get; set; }
+        [Required(ErrorMessage = " Challan No is mandatory")]
         public string ChallanNo { get; set; }
+        [Required(ErrorMessage = " Amount is mandatory")]
         public decimal? Amount { get; set; }
+        [Required(ErrorMessage = " Date of Payment is mandatory")]
         public DateTime? DateofPaymentByAllottee { get; set; }
+        [Required(ErrorMessage = " Proof of payment is mandatory")]
         public string Proofinpdf { get; set; }
         public string Ddabankcredit { get; set; }
         public byte IsActive { get; set; }
