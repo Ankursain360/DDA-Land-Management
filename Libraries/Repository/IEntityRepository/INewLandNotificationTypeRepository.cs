c6ef594361232e57ae86633d9e0c73035c4c44a1
Pankaj Bhandari <Pankaj.bhandari@vedng.net>
2021-08-19 15:40:08 +0530

    Added New Land Notification Page in Site Master.

---
+++ Libraries/Repository/IEntityRepository/INewLandNotificationTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dto.Search;
+using Libraries.Model.Entity;
+using Libraries.Repository.Common;
+
+namespace Libraries.Repository.IEntityRepository
+{
+    public interface INewLandNotificationTypeRepository : IGenericRepository<NewlandNotificationtype>
+    {
+
+        Task<PagedResult<NewlandNotificationtype>> GetPagedZone(NewLandNotificationTypeSearchDto model);
+
+    }
+}
---
+++ SiteMaster/Controllers/NewLandNotificationTypeController.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Libraries.Model.Entity;
+using Libraries.Service.IApplicationService;
+using SiteMaster.Models;
+using Notification;
+using Notification.Constants;
+using Notification.OptionEnums;
+using Microsoft.AspNetCore.Authorization;
+using Dto.Search;
+using SiteMaster.Filters;
+using Core.Enum;
+using System.Collections.Generic;
+using Utility.Helper;
+using Dto.Master;
+
+namespace SiteMaster.Controllers
+{ 
+    public class NewLandNotificationTypeController : BaseController
+    {
+        private readonly INewLandNotificationTypeService _newLandNotificationTypeService;
+
+        public NewLandNotificationTypeController(INewLandNotificationTypeService newLandNotificationTypeService)
+        {
+            _newLandNotificationTypeService = newLandNotificationTypeService;
+        }
+     
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public async Task<PartialViewResult> List([FromBody] NewLandNotificationTypeSearchDto model)
+        {
+            var result = await _newLandNotificationTypeService.GetPagedNotification(model);
+            return PartialView("_List", result);
+        }
+
+
+       
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NewlandNotificationtype notification)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var result = await _newLandNotificationTypeService.Create(notification);
+
+                    if (result == true)
+                    {
+                        ViewBag.Message = Alert.Show(Messages.AddRecordSuccess, "", AlertType.Success);
+                        var list = await _newLandNotificationTypeService.GetAllNotificationType();
+                        return View("Index", list);
+                    }
+                    else
+                    {
+                        ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                        return View(notification);
+
+                    }
+                }
+                else
+                {
+                    return View(notification);
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                return View(notification);
+            }
+        }
+
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var Data = await _newLandNotificationTypeService.FetchSingleResult(id);
+            if (Data == null)
+            {
+                return NotFound();
+            }
+            return View(Data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, NewlandNotificationtype notification)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var result = await _newLandNotificationTypeService.Update(id, notification);
+                    if (result == true)
+                    {
+                        ViewBag.Message = Alert.Show(Messages.UpdateRecordSuccess, "", AlertType.Success);
+                        var result1 = await _newLandNotificationTypeService.GetAllNotificationType();
+                        return View("Index", result1);
+                    }
+                    else
+                    {
+                        ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                        return View(notification);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            return View(notification);
+        }
+        public async Task<IActionResult> DeleteConfirmed(int id)  // Used to Perform Delete Functionality added by Renu
+        {
+            var result = await _newLandNotificationTypeService.Delete(id);
+            if (result == true)
+            {
+                ViewBag.Message = Alert.Show(Messages.DeleteSuccess, "", AlertType.Success);
+                var result1 = await _newLandNotificationTypeService.GetAllNotificationType();
+                return View("Index", result1);
+            }
+            else
+            {
+                ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                var result1 = await _newLandNotificationTypeService.GetAllNotificationType();
+                return View("Index", result1);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var result = await _newLandNotificationTypeService.Delete(id);
+                if (result == true)
+                {
+                    ViewBag.Message = Alert.Show(Messages.DeleteSuccess, "", AlertType.Success);
+                }
+                else
+                {
+                    ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = Alert.Show(Messages.Error, "", AlertType.Warning);
+            }
+            var list = await _newLandNotificationTypeService.GetAllNotificationType();
+            return View("Index", list);
+        }
+
+
+
+        public async Task<IActionResult> View(int id)
+        {
+            var Data = await _newLandNotificationTypeService.FetchSingleResult(id);
+            if (Data == null)
+            {
+                return NotFound();
+            }
+            return View(Data);
+        }
+
+        public async Task<IActionResult> NotificationList()
+        {
+            var result = await _newLandNotificationTypeService.GetAllNotificationType();
+            List<NotificationListDto> data = new List<NotificationListDto>();
+            if (result != null)
+            {
+                for (int i = 0; i < result.Count; i++)
+                {
+                    data.Add(new NotificationListDto()
+                    {
+                        Id = result[i].Id,
+                        Notification = result[i].NotificationType,
+
+                        Status = result[i].IsActive.ToString() == "1" ? "Active" : "Inactive",
+                    });
+                }
+            }
+
+            var memory = ExcelHelper.CreateExcel(data);
+            return File(memory, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        }
+
+    }
+}
