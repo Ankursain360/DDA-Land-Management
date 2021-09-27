bcc323c2208983819201ca58dce54dc56c6818b0
Pankaj Bhandari <Pankaj.bhandari@vedng.net>
2021-09-27 16:14:48 +0530

    Updated js updated on new lims and change edit in old lims

--- NewLandAcquisition/wwwroot/js/newlanddemandlistdetailsedit.js
+++ NewLandAcquisition/wwwroot/js/newlanddemandlistdetailsedit.js
@@ -153,193 +153,111 @@ function FillPaymentAtEdit() {
 
         $("#PaymentProofDocumentIFormFile").val(data.paymentProofDocumentIFormFile);
 
+      
+    }); 
+   
+}
+$('.checkExtension').on('change', function (e) {
 
-    });
+    debugger;
+    var flag = false;
+    var result = $(this).val();
+    var file = result;
+    if (file != null) {
 
-    $('.checkExtension').on('change', function (e) {
+        var multi = file.split(".");
+        if (multi.length > 2) {
 
-        debugger;
-        var flag = false;
-        var result = $(this).val();
-        var file = result;
-        if (file != null) {
+            alert("Please upload proper file with single dot in filename");
+            $(this).val('');
+            return;
+        }
+        var extension = file.substr((file.lastIndexOf('.') + 1));
 
-            var multi = file.split(".");
-            if (multi.length > 2) {
-
-                alert("Please upload proper file with single dot in filename");
+        switch (extension) {
+            case 'pdf':
+                flag = true;
+                $('#error').empty();
+                break;
+            case 'PDF':
+                flag = true;
+                $('#error').empty();
+                break;
+            default:
+                alert("You can upload only pdf extension file Only")
                 $(this).val('');
-                return;
-            }
-            var extension = file.substr((file.lastIndexOf('.') + 1));
-
-            switch (extension) {
-                case 'pdf':
-                    flag = true;
-                    $('#error').empty();
-                    break;
-                case 'PDF':
-                    flag = true;
-                    $('#error').empty();
-                    break;
-                default:
-                    alert("You can upload only pdf extension file Only")
-                    $(this).val('');
-                    flag = false;
-            }
-
-
-            if (flag == true) {
-
-                var FileID = $(this).attr('id');
-
-                var size = ValidateFileSize(FileID, $(this));
-
-                if (size > 5) {
-                    alert("You Can Upload file Size Up to 5 MB.");
-                    $(this).val('');
-                }
-                else {
-
-                    filecontrol = $(this);
-                    var myformData = new FormData();
-                    myformData.append('file', $(this)[0].files[0]);
-                    $.ajax({
-                        async: false,
-                        type: "POST",
-                        url: "/NewLandDemandListDetails/CheckFile",
-                        contentType: false,
-                        processData: false,
-                        data: myformData,
-                        success: function (response) {
-
-                            showResult(response, filecontrol)
-
-                        },
-                        failure: function (response) {
-                            //alert(response.d);
-                            return false;
-                        }
-                    });
-                    function showResult(response, filecontrol) {
-                        debugger;
-                        if (response == false) {
-                            alert("Please select vaild pdf file.");
-                            filecontrol.val('');
-                        }
-                        else {
-                            return true;
-                        }
-                    }
-
-                }
-            }
+                flag = false;
         }
 
 
-    });
-    $('.checkExtension1').on('change', function (e) {
+        if (flag == true) {
 
-        debugger;
-        var flag = false;
-        var result = $(this).val();
-        var file = result;
-        if (file != null) {
+            var FileID = $(this).attr('id');
 
-            var multi = file.split(".");
-            if (multi.length > 2) {
+            var size = checksize($(this));
 
-                alert("Please upload proper file with single dot in filename");
+            if (size > 5) {
+                alert("You Can Upload file Size Up to 5 MB.");
                 $(this).val('');
-                return;
-            }
-            var extension = file.substr((file.lastIndexOf('.') + 1));
-
-            switch (extension) {
-                case 'pdf':
-                    flag = true;
-                    $('#error').empty();
-                    break;
-                case 'PDF':
-                    flag = true;
-                    $('#error').empty();
-                    break;
-                default:
-                    alert("You can upload only pdf extension file Only")
-                    $(this).val('');
-                    flag = false;
-            }
-
-
-            if (flag == true) {
-
-                var FileID = $(this).attr('id');
-
-                var size = ValidateFileSize(FileID, $(this));
-
-                if (size > 5) {
-                    alert("You Can Upload file Size Up to 5 MB.");
-                    $(this).val('');
-                }
-                else {
-
-                    filecontrol = $(this);
-                    var myformData = new FormData();
-                    myformData.append('file', $(this)[0].files[0]);
-                    $.ajax({
-                        async: false,
-                        type: "POST",
-                        url: "/NewLandDemandListDetails/CheckFile1",
-                        contentType: false,
-                        processData: false,
-                        data: myformData,
-                        success: function (response) {
-
-                            showResult(response, filecontrol)
-
-                        },
-                        failure: function (response) {
-                            //alert(response.d);
-                            return false;
-                        }
-                    });
-                    function showResult(response, filecontrol) {
-                        debugger;
-                        if (response == false) {
-                            alert("Please select vaild pdf file.");
-                            filecontrol.val('');
-                        }
-                        else {
-                            return true;
-                        }
-                    }
-
-                }
-            }
-        }
-
-
-    });
-
-    function ValidateFileSize(fileid, file) {
-        try {
-            var fileSize = 0;
-            if (navigator.userAgent.match(/msie/i)) {
-                var obaxo = new ActiveXObject("Scripting.FileSystemObject");
-                var filePath = file[0].value;
-                var objFile = obaxo.getFile(filePath);
-                var fileSize = objFile.size;
-                fileSize = fileSize / 1048576;
             }
             else {
-                fileSize = file[0].files[0].size
-                fileSize = fileSize / 1048576;
-            }
 
-            return fileSize;
+                filecontrol = $(this);
+                var myformData = new FormData();
+                myformData.append('file', $(this)[0].files[0]);
+                $.ajax({
+                    async: false,
+                    type: "POST",
+                    url: "/NewLandDemandListDetails/CheckFile",
+                    contentType: false,
+                    processData: false,
+                    data: myformData,
+                    success: function (response) {
+
+                        showResult(response, filecontrol)
+
+                    },
+                    failure: function (response) {
+                        //alert(response.d);
+                        return false;
+                    }
+                });
+                function showResult(response, filecontrol) {
+                    debugger;
+                    if (response == false) {
+                        alert("Please select vaild pdf file.");
+                        filecontrol.val('');
+                    }
+                    else {
+                        return true;
+                    }
+                }
+
+            }
         }
-        catch (e) {
-            alert("Error is :" + e);
+    }
+
+
+});
+
+function checksize(file) {
+    try {
+        var fileSize = 0;
+        if (navigator.userAgent.match(/msie/i)) {
+            var obaxo = new ActiveXObject("Scripting.FileSystemObject");
+            var filePath = file[0].value;
+            var objFile = obaxo.getFile(filePath);
+            var fileSize = objFile.size;
+            fileSize = fileSize / 1048576;
         }
+        else {
+            fileSize = file[0].files[0].size
+            fileSize = fileSize / 1048576;
+        }
+
+        return fileSize;
+    }
+    catch (e) {
+        alert("Error is :" + e);
     }
 }
