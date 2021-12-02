597e5d8fec33412e9ddc4e99551cea73f7b62c82
Ishu-rana <69201934+Ishu-rana@users.noreply.github.com>
2021-12-02 17:18:25 +0530

    DuesVsPaidAmount report changes done

--- Libraries/Dto/Master/DuesVsPaidAmountDto.cs
+++ Libraries/Dto/Master/DuesVsPaidAmountDto.cs
@@ -6,17 +6,20 @@ namespace Dto.Master
    public class DuesVsPaidAmountDto
     {
 
-      //  public int P_filenoId { get; set; }
+        //  public int P_filenoId { get; set; }
+        public string LocalityName { get; set; }
         public string FileNo { get; set; }
         public string Name { get; set; }
 
         public string Address { get; set; }
         public string DemandNo { get; set; }
+        public string DamageCharges { get; set; }
+        public decimal Penalty { get; set; }
         public string InterestAmount { get; set; }
-        public string DepositDue { get; set; }
         public decimal ReliefAmount { get; set; }
-        public double demand_amount { get; set; }
-        public double balance_amt { get; set; }
+        public string DemandAmount { get; set; }
+        public decimal PaidAmount { get; set; }
+        public double BalanceAmount { get; set; }
 
 
     }
