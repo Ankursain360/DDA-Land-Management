dbe2faa8a781bd38172ddce3951327f7546e37c3
Pankaj <pankaj.bhandari@vedang.net>
2022-02-23 17:40:38 +0530

    Site master bug resolved in Interest and Rebate

--- Libraries/Model/Entity/Interest.cs
+++ Libraries/Model/Entity/Interest.cs
@@ -9,16 +9,16 @@ namespace Libraries.Model.Entity
 {
     public class Interest : AuditableEntity<int>
     {
-        [Required(ErrorMessage = "The Property Type field is required", AllowEmptyStrings = false)]
+        [Required(ErrorMessage = "Property Field is Mandatory", AllowEmptyStrings = false)]
         public int PropertyId { get; set; }
 
-        [Required(ErrorMessage = "The From Date field is required")]
+        [Required(ErrorMessage = "From Date Field is Mandatory")]
         public DateTime FromDate { get; set; }
 
-        [Required(ErrorMessage = "The To Date field is required")]
+        [Required(ErrorMessage = "To Date Field is Mandatory")]
         public DateTime ToDate { get; set; }
 
-        [Required(ErrorMessage = "The Percentage field is required")]
+        [Required(ErrorMessage = "Percentage Field is Mandatory")]
         [RegularExpression(@"((\d+)((\.\d{1,3})?))$", ErrorMessage = "Please enter valid integer or decimal number with 3 decimal places.")]
         [Range(0, 9999999999999999.99, ErrorMessage = "Invalid Percentage; Max 18 digits")]
         public decimal Percentage { get; set; }
