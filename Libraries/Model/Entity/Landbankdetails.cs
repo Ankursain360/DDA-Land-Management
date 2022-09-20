1177a8e42926068b1ca2c6752452de92e7019d69
Sachin bhatt <sachin.bhatt89@outlook.com>
2022-09-20 12:28:18 +0530

    sync

--- Libraries/Model/Entity/Landbankdetails.cs
+++ Libraries/Model/Entity/Landbankdetails.cs
@@ -4,9 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Model.Entity
+namespace Libraries.Model.Entity
 {
-    public class Landbankdetails:AuditableEntity<int>
+    public class Landbankdetails : AuditableEntity<int>
     {
         public string ZoneName { get; set; }
         public string VillageName { get; set; }
