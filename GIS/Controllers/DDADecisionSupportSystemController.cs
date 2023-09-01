f4f7bc863b4836b87335586d8644e9e51e8b171e
Administrator <Administrator@ACERLAP>
2023-09-01 14:54:50 +0530

    GIS NEW PAGES

---
+++ GIS/Controllers/DDADecisionSupportSystemController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GIS.Controllers
+{
+    public class DDADecisionSupportSystemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
