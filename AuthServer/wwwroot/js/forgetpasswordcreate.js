b6d2ae63b1c27f32c0ee3af487bf5862af77811e
Ankursain360 <rzakthakur@gmail.com>
2024-05-15 10:16:43 +0530

    validation on valid password in ResetPassword

--- AuthServer/wwwroot/js/forgetpasswordcreate.js
+++ AuthServer/wwwroot/js/forgetpasswordcreate.js
@@ -6,3 +6,17 @@ function resetCaptchaImage() {
     d = new Date();
     $("#img-captcha").attr("src", "/get-captcha-image?" + d.getTime());
 }
+
+function CheckPasswordValidation() {
+    debugger;
+    var checkresult = false;
+    let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+    var password = document.getElementById("Password").value
+    if (regex.test(password)) {
+        checkresult = true;
+        return checkresult;
+    }
+    else {
+        alert('Passwords must contain at least eight characters, including uppercase, lowercase letters,numbers and special character (@#$!%*?&)')
+    }
+}
